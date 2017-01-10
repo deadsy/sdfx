9369a42528aebb8c98beb381ea74958a4b020f56
Jason Harris <jasonh@mistsys.com>
2017-01-09 16:33:24 -0800

    Added the rest of the hg_sdf library. Still commented out.

--- sdf/sdf.go
+++ sdf/sdf.go
@@ -406,3 +406,147 @@ float pReflect(inout vec3 p, vec3 planeNormal, float offset) {
 */
 
 //-----------------------------------------------------------------------------
+// Object Combination Operators
+
+/*
+
+// The "Chamfer" flavour makes a 45-degree chamfered edge (the diagonal of a square of size <r>):
+float fOpUnionChamfer(float a, float b, float r) {
+	return min(min(a, b), (a - r + b)*sqrt(0.5));
+}
+
+// Intersection has to deal with what is normally the inside of the resulting object
+// when using union, which we normally don't care about too much. Thus, intersection
+// implementations sometimes differ from union implementations.
+float fOpIntersectionChamfer(float a, float b, float r) {
+	return max(max(a, b), (a + r + b)*sqrt(0.5));
+}
+
+// Difference can be built from Intersection or Union:
+float fOpDifferenceChamfer (float a, float b, float r) {
+	return fOpIntersectionChamfer(a, -b, r);
+}
+
+// The "Round" variant uses a quarter-circle to join the two objects smoothly:
+float fOpUnionRound(float a, float b, float r) {
+	vec2 u = max(vec2(r - a,r - b), vec2(0));
+	return max(r, min (a, b)) - length(u);
+}
+
+float fOpIntersectionRound(float a, float b, float r) {
+	vec2 u = max(vec2(r + a,r + b), vec2(0));
+	return min(-r, max (a, b)) + length(u);
+}
+
+float fOpDifferenceRound (float a, float b, float r) {
+	return fOpIntersectionRound(a, -b, r);
+}
+
+
+// The "Columns" flavour makes n-1 circular columns at a 45 degree angle:
+float fOpUnionColumns(float a, float b, float r, float n) {
+	if ((a < r) && (b < r)) {
+		vec2 p = vec2(a, b);
+		float columnradius = r*sqrt(2)/((n-1)*2+sqrt(2));
+		pR45(p);
+		p.x -= sqrt(2)/2*r;
+		p.x += columnradius*sqrt(2);
+		if (mod(n,2) == 1) {
+			p.y += columnradius;
+		}
+		// At this point, we have turned 45 degrees and moved at a point on the
+		// diagonal that we want to place the columns on.
+		// Now, repeat the domain along this direction and place a circle.
+		pMod1(p.y, columnradius*2);
+		float result = length(p) - columnradius;
+		result = min(result, p.x);
+		result = min(result, a);
+		return min(result, b);
+	} else {
+		return min(a, b);
+	}
+}
+
+float fOpDifferenceColumns(float a, float b, float r, float n) {
+	a = -a;
+	float m = min(a, b);
+	//avoid the expensive computation where not needed (produces discontinuity though)
+	if ((a < r) && (b < r)) {
+		vec2 p = vec2(a, b);
+		float columnradius = r*sqrt(2)/n/2.0;
+		columnradius = r*sqrt(2)/((n-1)*2+sqrt(2));
+
+		pR45(p);
+		p.y += columnradius;
+		p.x -= sqrt(2)/2*r;
+		p.x += -columnradius*sqrt(2)/2;
+
+		if (mod(n,2) == 1) {
+			p.y += columnradius;
+		}
+		pMod1(p.y,columnradius*2);
+
+		float result = -length(p) + columnradius;
+		result = max(result, p.x);
+		result = min(result, a);
+		return -min(result, b);
+	} else {
+		return -m;
+	}
+}
+
+float fOpIntersectionColumns(float a, float b, float r, float n) {
+	return fOpDifferenceColumns(a,-b,r, n);
+}
+
+// The "Stairs" flavour produces n-1 steps of a staircase:
+// much less stupid version by paniq
+float fOpUnionStairs(float a, float b, float r, float n) {
+	float s = r/n;
+	float u = b-r;
+	return min(min(a,b), 0.5 * (u + a + abs ((mod (u - a + s, 2 * s)) - s)));
+}
+
+// We can just call Union since stairs are symmetric.
+float fOpIntersectionStairs(float a, float b, float r, float n) {
+	return -fOpUnionStairs(-a, -b, r, n);
+}
+
+float fOpDifferenceStairs(float a, float b, float r, float n) {
+	return -fOpUnionStairs(-a, b, r, n);
+}
+
+
+// Similar to fOpUnionRound, but more lipschitz-y at acute angles
+// (and less so at 90 degrees). Useful when fudging around too much
+// by MediaMolecule, from Alex Evans' siggraph slides
+float fOpUnionSoft(float a, float b, float r) {
+	float e = max(r - abs(a - b), 0);
+	return min(a, b) - e*e*0.25/r;
+}
+
+
+// produces a cylindical pipe that runs along the intersection.
+// No objects remain, only the pipe. This is not a boolean operator.
+float fOpPipe(float a, float b, float r) {
+	return length(vec2(a, b)) - r;
+}
+
+// first object gets a v-shaped engraving where it intersect the second
+float fOpEngrave(float a, float b, float r) {
+	return max(a, (a + r - abs(b))*sqrt(0.5));
+}
+
+// first object gets a capenter-style groove cut out
+float fOpGroove(float a, float b, float ra, float rb) {
+	return max(a, min(a + ra, rb - abs(b)));
+}
+
+// first object gets a capenter-style tongue attached
+float fOpTongue(float a, float b, float ra, float rb) {
+	return min(a, max(a - ra, abs(b) - rb));
+}
+
+*/
+
+//-----------------------------------------------------------------------------
