f6a7a65118a877f52d5e9d104443cbb4e2a4bd29
Jason Harris <jasonh@mistsys.com>
2017-02-09 17:11:06 -0800

    Working on a 2D polygon that can memoize vertex distances and avoid some work.

--- sdf/sdf2.go
+++ sdf/sdf2.go
@@ -247,6 +247,106 @@ func (s *PolySDF2) BoundingBox() Box2 {
 }
 
 //-----------------------------------------------------------------------------
+// 2D Polygon the Next Generation
+
+type NGPolySDF2 struct {
+	vertex []V2      // vertices
+	vector []V2      // unit line vectors
+	normal []V2      // unit line normals (to the right, anticlockwise order)
+	length []float64 // line lengths
+	bb     Box2      // bounding box
+}
+
+func NewNGPolySDF2(vertex []V2) SDF2 {
+	s := NGPolySDF2{}
+
+	n := len(vertex)
+	if n < 3 {
+		return nil
+	}
+
+	// Close the loop (if necessary)
+	s.vertex = vertex
+	if !vertex[0].Equals(vertex[n-1], 0) {
+		s.vertex = append(s.vertex, vertex[0])
+	}
+
+	// allocate pre-calculated line segment info
+	nsegs := len(s.vertex) - 1
+	s.vector = make([]V2, nsegs)
+	s.normal = make([]V2, nsegs)
+	s.length = make([]float64, nsegs)
+
+	vmin := s.vertex[0]
+	vmax := s.vertex[0]
+
+	for i := 0; i < nsegs; i++ {
+		l := s.vertex[i+1].Sub(s.vertex[i])
+		s.length[i] = l.Length()
+		s.vector[i] = l.Normalize()
+		s.normal[i] = V2{s.vector[i].Y, -s.vector[i].X}
+		vmin = vmin.Min(s.vertex[i])
+		vmax = vmax.Max(s.vertex[i])
+	}
+
+	s.bb = Box2{vmin, vmax}
+	return &s
+}
+
+func (s *NGPolySDF2) Evaluate(p V2) float64 {
+	dd := math.MaxFloat64 // d^2 to polygon (>0)
+	wn := 0               // winding number (inside/outside)
+
+	// iterate over the line segments
+	nsegs := len(s.vertex) - 1
+	for i := 0; i < nsegs; i++ {
+		a := s.vertex[i]
+		b := s.vertex[i+1]
+		pa := p.Sub(a)
+		t := pa.Dot(s.vector[i])  // t-parameter of projection onto line
+		dn := pa.Dot(s.normal[i]) // normal distance from p to line
+
+		// Distance to line segment
+		if t < 0 {
+			dd = Min(dd, pa.Length2()) // distance to vertex[0] of line
+		} else if t > s.length[i] {
+			pb := p.Sub(b)
+			dd = Min(dd, pb.Length2()) // distance to vertex[1] of line
+		} else {
+			dd = Min(dd, dn*dn) // normal distance to line
+		}
+
+		// Is the point in the polygon?
+		// See: http://geomalgorithms.com/a03-_inclusion.html
+		if a.Y <= p.Y {
+			if b.Y > p.Y { // upward crossing
+				if dn < 0 { // p is to the left of the line segment
+					wn += 1 // up intersect
+				}
+			}
+		} else {
+			if b.Y <= p.Y { // downward crossing
+				if dn > 0 { // p is to the right of the line segment
+					wn -= 1 // down intersect
+				}
+			}
+		}
+	}
+
+	// normalise d*d to d
+	d := math.Sqrt(dd)
+	if wn != 0 {
+		// p is inside the polygon
+		return -d
+	}
+	return d
+}
+
+func (s *NGPolySDF2) BoundingBox() Box2 {
+	return s.bb
+}
+
+//-----------------------------------------------------------------------------
 // Transform SDF2
 
 type TransformSDF2 struct {
