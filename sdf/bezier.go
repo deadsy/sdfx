ddb5954e95518a4fa64fa9a69100060807e77398
Jason Harris <jasonh@mistsys.com>
2017-03-24 10:13:57 -0700

    Added quintic bezier polynomials.

--- sdf/bezier.go
+++ sdf/bezier.go
@@ -17,53 +17,72 @@ const POLY_EPSILON = 1e-12
 //-----------------------------------------------------------------------------
 
 type BezierPolynomial struct {
-	n          int     // polynomial order
-	a, b, c, d float64 // polynomial coefficients
+	n             int     // polynomial order
+	a, b, c, d, e float64 // polynomial coefficients
 }
 
+// Return the bezier polynomial function value.
 func (p *BezierPolynomial) f0(t float64) float64 {
 	switch p.n {
 	case 1:
+		// linear
 		return p.a + t*p.b
 	case 2:
+		// quadratic
 		return p.a + t*(p.b+t*p.c)
 	case 3:
-		return p.a + t*(p.b+t*(p.c+p.d*t))
+		// cubic
+		return p.a + t*(p.b+t*(p.c+t*p.d))
+	case 4:
+		// quintic
+		return p.a + t*(p.b+t*(p.c+t*(p.d+t*p.e)))
 	default:
 		panic(fmt.Sprintf("bad polynomial order %d", p.n))
 	}
-	return 0
 }
 
+// Given the end/control points calculate the polynomial coefficients.
 func (p *BezierPolynomial) Set(x []float64) {
 	p.n = len(x) - 1
 	switch p.n {
 	case 1:
+		// linear
 		p.a = x[0]
-		p.b = x[1] - x[0]
+		p.b = -x[0] + x[1]
 	case 2:
+		// quadratic
 		p.a = x[0]
-		p.b = 2 * (x[1] - x[0])
-		p.c = x[2] - 2*x[1] + x[0]
+		p.b = -2*x[0] + 2*x[1]
+		p.c = x[0] - 2*x[1] + x[2]
 	case 3:
+		// cubic
 		p.a = x[0]
-		p.b = 3 * (x[1] - x[0])
-		p.c = 3 * (x[2] - 2*x[1] + x[0])
-		p.d = x[3] - 3*x[2] + 3*x[1] - x[0]
+		p.b = -3*x[0] + 3*x[1]
+		p.c = 3*x[0] - 6*x[1] + 3*x[2]
+		p.d = -x[0] + 3*x[1] - 3*x[2] + x[3]
+	case 4:
+		// quintic
+		p.a = x[0]
+		p.b = -4*x[0] + 4*x[1]
+		p.c = 6*x[0] - 12*x[1] + 6*x[2]
+		p.d = -4*x[0] + 12*x[1] - 12*x[2] + 4*x[3]
+		p.e = x[0] - 4*x[1] + 6*x[2] - 4*x[3] + x[4]
 	default:
 		panic(fmt.Sprintf("bad polynomial order %d", p.n))
 	}
-	sum := Abs(p.a) + Abs(p.b) + Abs(p.c) + Abs(p.d)
+	// zero out any very small coefficients
+	sum := Abs(p.a) + Abs(p.b) + Abs(p.c) + Abs(p.d) + Abs(p.e)
 	p.a = ZeroSmall(p.a, sum, POLY_EPSILON)
 	p.b = ZeroSmall(p.b, sum, POLY_EPSILON)
 	p.c = ZeroSmall(p.c, sum, POLY_EPSILON)
 	p.d = ZeroSmall(p.d, sum, POLY_EPSILON)
+	p.e = ZeroSmall(p.e, sum, POLY_EPSILON)
 }
 
 //-----------------------------------------------------------------------------
 
 type BezierSpline struct {
-	px, py BezierPolynomial // bezier polynomial
+	px, py BezierPolynomial // x/y bezier polynomials
 }
 
 // Return the function value for a given t value.
