c56bad45937e255652b162fedd8dc653f08af263
Jason Harris <jasonh@mistsys.com>
2017-06-03 08:03:43 -0700

    Added another dust collection adapter.

--- examples/dust_collection/main.go
+++ examples/dust_collection/main.go
@@ -18,24 +18,27 @@ import (
 
 // dust deputy tapered pipe
 var dd_outer_d = 51.0
-var dd_length = 39.0
 var dd_taper = DtoR(2.0)
 
 // vaccum hose 2.5" male fitting
 var vh_outer_d = 58.0
-var vh_length = 30.0
-var vh_clearance = 0.5
-var vh_taper = DtoR(0.5)
+var vh_clearance = 0.6
+var vh_taper = DtoR(0.4)
+
+// pvc 3"
+var pvc3_outer_d = 3.26 * MM_PER_INCH
 
 var wall_thickness = 4.0
-var transition_length = 15.0
 
 //-----------------------------------------------------------------------------
 
-// dust deputy to 2.5" vacuum hose
-func dd_to_hose25() {
+// adapter: female dust deputy, female 2.5" vacuum
+func fdd_to_fvh25() {
 
 	t := wall_thickness
+	transition_length := 15.0
+	vh_length := 30.0
+	dd_length := 39.0
 
 	r0 := dd_outer_d / 2
 	r1 := r0 - dd_length*math.Tan(dd_taper)
@@ -43,7 +46,7 @@ func dd_to_hose25() {
 	r2 := r3 - (vh_length * math.Tan(vh_taper))
 
 	h0 := 0.0
-	h1 := dd_length
+	h1 := h0 + dd_length
 	h2 := h1 + transition_length
 	h3 := h2 + vh_length
 
@@ -60,13 +63,46 @@ func dd_to_hose25() {
 	s_2d := Polygon2D(p.Vertices())
 	s_3d := Revolve3D(s_2d)
 
-	RenderSTL(s_3d, 300, "adapter1.stl")
+	RenderSTL(s_3d, 150, "fdd_fvh25.stl")
+}
+
+//-----------------------------------------------------------------------------
+
+// adapter: male 2.5" vacuum, male 3" pvc
+func mvh25_to_mpvc3() {
+
+	t := wall_thickness
+	transition_length := 15.0
+
+	r0 := pvc3_outer_d / 2
+	r1 := vh_outer_d / 2
+
+	h0 := 0.0
+	h1 := h0 + 35.0
+	h2 := h1 + transition_length
+	h3 := h2 + 20.0
+
+	p := NewPolygon()
+	p.Add(r0+t, h0)
+	p.Add(r0+t, h1).Smooth(t, 4)
+	p.Add(r1+t, h2).Smooth(t, 4)
+	p.Add(r1+t, h3)
+	p.Add(r1, h3)
+	p.Add(r1, h2).Smooth(t, 4)
+	p.Add(r0, h1).Smooth(t, 4)
+	p.Add(r0, h0)
+
+	s_2d := Polygon2D(p.Vertices())
+	s_3d := Revolve3D(s_2d)
+
+	RenderSTL(s_3d, 150, "mvh25_mpvc3.stl")
 }
 
 //-----------------------------------------------------------------------------
 
 func main() {
-	dd_to_hose25()
+	fdd_to_fvh25()
+	mvh25_to_mpvc3()
 }
 
 //-----------------------------------------------------------------------------
