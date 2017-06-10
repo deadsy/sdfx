15860b348a25a7ac3cd9b0421c00ff825813901f
Jason Harris <jasonh@mistsys.com>
2017-06-10 12:11:21 -0700

    Changed the pipe to 2" pvc.

--- examples/dust_collection/main.go
+++ examples/dust_collection/main.go
@@ -17,17 +17,18 @@ import (
 //-----------------------------------------------------------------------------
 
 // dust deputy tapered pipe
-var dd_outer_d = 51.0
+var dd_od = 51.0
 var dd_taper = DtoR(2.0)
 var dd_length = 39.0
 
 // vaccum hose 2.5" male fitting
-var vh_outer_d = 58.0
+var vh_od = 58.0
 var vh_clearance = 0.6
 var vh_taper = DtoR(0.4)
 
-// pvc 3"
-var pvc3_outer_d = 3.26 * MM_PER_INCH
+// pvc pipe outside diameters
+var pvc3_od = 3.26 * MM_PER_INCH
+var pvc2_od = 2.375 * MM_PER_INCH
 
 var wall_thickness = 4.0
 
@@ -40,9 +41,9 @@ func fdd_to_fvh25() {
 	transition_length := 15.0
 	vh_length := 30.0
 
-	r0 := dd_outer_d / 2
+	r0 := dd_od / 2
 	r1 := r0 - dd_length*math.Tan(dd_taper)
-	r3 := (vh_outer_d + vh_clearance) / 2
+	r3 := (vh_od + vh_clearance) / 2
 	r2 := r3 - (vh_length * math.Tan(vh_taper))
 
 	h0 := 0.0
@@ -67,13 +68,13 @@ func fdd_to_fvh25() {
 //-----------------------------------------------------------------------------
 
 // adapter: male 2.5" vacuum, male 3" pvc
-func mvh25_to_mpvc3() {
+func mvh25_to_mpvc(pvc_od float64) {
 
 	t := wall_thickness
 	transition_length := 15.0
 
-	r0 := pvc3_outer_d / 2
-	r1 := vh_outer_d / 2
+	r0 := pvc_od / 2
+	r1 := vh_od / 2
 
 	h0 := 0.0
 	h1 := h0 + 35.0
@@ -91,19 +92,19 @@ func mvh25_to_mpvc3() {
 	p.Add(r0-t, h0)
 
 	s := Revolve3D(Polygon2D(p.Vertices()))
-	RenderSTL(s, 150, "mvh25_mpvc3.stl")
+	RenderSTL(s, 150, "mvh25_mpvc.stl")
 }
 
 //-----------------------------------------------------------------------------
 
 // adapter: female dust deputy, male 3" pvc
-func fdd_to_mpvc3() {
+func fdd_to_mpvc(pvc_od float64) {
 
 	t := wall_thickness
 	transition_length := 15.0
 
-	r0 := pvc3_outer_d / 2
-	r2 := (dd_outer_d / 2) + t
+	r0 := pvc_od / 2
+	r2 := (dd_od / 2) + t
 	r1 := r2 - dd_length*math.Tan(dd_taper)
 
 	h0 := 0.0
@@ -122,15 +123,15 @@ func fdd_to_mpvc3() {
 	p.Add(r0-t, h0)
 
 	s := Revolve3D(Polygon2D(p.Vertices()))
-	RenderSTL(s, 150, "fdd_mpvc3.stl")
+	RenderSTL(s, 150, "fdd_mpvc.stl")
 }
 
 //-----------------------------------------------------------------------------
 
 func main() {
 	fdd_to_fvh25()
-	mvh25_to_mpvc3()
-	fdd_to_mpvc3()
+	mvh25_to_mpvc(pvc2_od)
+	fdd_to_mpvc(pvc2_od)
 }
 
 //-----------------------------------------------------------------------------
