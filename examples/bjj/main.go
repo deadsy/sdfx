bac575dac245bd277ae9ba61f8ec33bb4f41f5eb
Jason Harris <sirmanlypowers@gmail.com>
2018-04-02 08:12:31 -0700

    Adding stacked gears to bjj.

--- examples/bjj/main.go
+++ examples/bjj/main.go
@@ -14,19 +14,17 @@ import . "github.com/deadsy/sdfx/sdf"
 
 //-----------------------------------------------------------------------------
 
-// R6-2RS 3/8 x 7/8 x 9/32 bearing
-var bearing_outer_od = (7.0 / 8.0) * MM_PER_INCH   // outer diameter of outer race
-var bearing_outer_id = 19.0                        // inner diameter of outer race
-var bearing_inner_id = (3.0 / 8.0) * MM_PER_INCH   // inner diameter of inner race
-var bearing_inner_od = 12.0                        // outer diameter of inner race
-var bearing_thickness = (9.0 / 32.0) * MM_PER_INCH // bearing thickness
-
-// Adjust clearance to give good interference fits for the bearing
-var clearance = 0.0
-
-//-----------------------------------------------------------------------------
-
 func bushing() SDF3 {
+
+	// R6-2RS 3/8 x 7/8 x 9/32 bearing
+	bearing_outer_od := (7.0 / 8.0) * MM_PER_INCH // outer diameter of outer race
+	//bearing_outer_id := 19.0                        // inner diameter of outer race
+	bearing_inner_id := (3.0 / 8.0) * MM_PER_INCH   // inner diameter of inner race
+	bearing_inner_od := 12.0                        // outer diameter of inner race
+	bearing_thickness := (9.0 / 32.0) * MM_PER_INCH // bearing thickness
+
+	// Adjust clearance to give good interference fits for the bearing
+	clearance := 0.0
 
 	r0 := 2.3 // radius of central screw
 	r1 := (bearing_outer_od + bearing_inner_od) / 4.0
@@ -47,8 +45,50 @@ func bushing() SDF3 {
 
 //-----------------------------------------------------------------------------
 
+var gear_module = 80.0 / 16.0
+var pressure_angle = 20.0
+var involute_facets = 10
+
+func stacked_gears() SDF3 {
+
+	sg_height := 10.0
+	sg0_teeth := 12
+	sg1_teeth := 16
+
+	g0_2d := InvoluteGear(
+		sg0_teeth,
+		gear_module,
+		DtoR(pressure_angle),
+		0.0,
+		0.0,
+		20.0, // width of ring wall (from root circle)
+		involute_facets,
+	)
+
+	g1_2d := InvoluteGear(
+		sg1_teeth,
+		gear_module,
+		DtoR(pressure_angle),
+		0.0,
+		0.0,
+		20.0, // width of ring wall (from root circle)
+		involute_facets,
+	)
+
+	g0 := Extrude3D(g0_2d, sg_height)
+	g1 := Extrude3D(g1_2d, sg_height)
+
+	g0 = Transform3D(g0, Translate3d(V3{0, 0, sg_height / 2.0}))
+	g1 = Transform3D(g1, Translate3d(V3{0, 0, -sg_height / 2.0}))
+
+	return Union3D(g0, g1)
+}
+
+//-----------------------------------------------------------------------------
+
 func main() {
 	RenderSTL(bushing(), 300, "bushing.stl")
+	RenderSTL(stacked_gears(), 300, "gear.stl")
 }
 
 //-----------------------------------------------------------------------------
