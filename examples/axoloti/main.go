9ffbbc81bd96dcc0f4cbfad6422e984b072f2d93
Jason Harris <sirmanlypowers@gmail.com>
2018-04-13 10:35:13 -0700

    Added finger buttons to the axoloti front panel.

--- examples/axoloti/main.go
+++ examples/axoloti/main.go
@@ -119,57 +119,47 @@ func base() SDF3 {
 
 //-----------------------------------------------------------------------------
 
-const (
-	CUTOUT_CIRCLE = iota
-	CUTOUT_RECT
-)
-
-type CutoutParms struct {
-	cutout_type int       // circle or rectangle
-	center      V2        // center of cutout
-	parms       []float64 // width, height or diameter
+type PanelHole struct {
+	center V2   // center of hole
+	hole   SDF2 // 2d hole
 }
 
 func front_panel() SDF3 {
+
+	s_midi := Circle2D(0.5 * 15.5)
+	s_jack := Circle2D(0.5 * 11.5)
+	s_led := Box2D(V2{1.6, 1.6}, 0)
+
+	fb := &FingerButtonParms{
+		Width:  3.5,
+		Gap:    0.5,
+		Length: 20.0,
+	}
+	s_button := Transform2D(FingerButton2D(fb), Rotate2d(DtoR(-90)))
 
 	jack_x := 123.0
 	midi_x := 18.2
 	led_x := 62.7
 	pb_x := 52.8
 
-	midi_d := 15.5
-	jack_d := 11.5
-
-	cp := []CutoutParms{
-		{CUTOUT_CIRCLE, V2{midi_x, 9.3}, []float64{midi_d}},         // MIDI DIN Jack
-		{CUTOUT_CIRCLE, V2{midi_x + 20.32, 9.3}, []float64{midi_d}}, // MIDI DIN Jack
-		{CUTOUT_CIRCLE, V2{jack_x, 8.14}, []float64{jack_d}},        // 1/4" Stereo Jack
-		{CUTOUT_CIRCLE, V2{jack_x + 19.5, 8.14}, []float64{jack_d}}, // 1/4" Stereo Jack
-		{CUTOUT_RECT, V2{led_x, 0.5}, []float64{1.6, 1.6}},          // LED
-		{CUTOUT_RECT, V2{led_x + 3.635, 0.5}, []float64{1.6, 1.6}},  // LED
-		{CUTOUT_RECT, V2{pb_x, 0.8}, []float64{3.5, 1.6}},           // Push Button
-		{CUTOUT_RECT, V2{pb_x + 5.334, 0.8}, []float64{3.5, 1.6}},   // Push Button
-
-		{CUTOUT_CIRCLE, V2{107.4, 2.3}, []float64{5.5}},    // 3.5 mm Headphone Jack
-		{CUTOUT_RECT, V2{84.1, 1.0}, []float64{14.3, 2.0}}, // micro SD card
-		{CUTOUT_RECT, V2{96.7, 1.3}, []float64{8.0, 3.1}},  // micro USB connector
-		{CUTOUT_RECT, V2{72.6, 7.6}, []float64{7.1, 14.8}}, // fullsize USB connector
+	holes := []PanelHole{
+		{V2{midi_x, 9.3}, s_midi},                // MIDI DIN Jack
+		{V2{midi_x + 20.32, 9.3}, s_midi},        // MIDI DIN Jack
+		{V2{jack_x, 8.14}, s_jack},               // 1/4" Stereo Jack
+		{V2{jack_x + 19.5, 8.14}, s_jack},        // 1/4" Stereo Jack
+		{V2{107.4, 2.3}, Circle2D(0.5 * 5.5)},    // 3.5 mm Headphone Jack
+		{V2{led_x, 0.5}, s_led},                  // LED
+		{V2{led_x + 3.635, 0.5}, s_led},          // LED
+		{V2{pb_x, 0.8}, s_button},                // Push Button
+		{V2{pb_x + 5.334, 0.8}, s_button},        // Push Button
+		{V2{84.1, 1.0}, Box2D(V2{14.3, 2.0}, 0)}, // micro SD card
+		{V2{96.7, 1.3}, Box2D(V2{8.0, 3.1}, 0)},  // micro USB connector
+		{V2{72.6, 7.6}, Box2D(V2{7.1, 14.8}, 0)}, // fullsize USB connector
 	}
 
-	s := make([]SDF2, len(cp))
-	for i, p := range cp {
-		var s0 SDF2
-		if p.cutout_type == CUTOUT_CIRCLE {
-			r := p.parms[0] / 2.0
-			s0 = Circle2D(r)
-		} else if p.cutout_type == CUTOUT_RECT {
-			w := p.parms[0]
-			h := p.parms[1]
-			s0 = Box2D(V2{w, h}, 0.0)
-		} else {
-			panic("bad cutout type")
-		}
-		s[i] = Transform2D(s0, Translate2d(p.center))
+	s := make([]SDF2, len(holes))
+	for i, k := range holes {
+		s[i] = Transform2D(k.hole, Translate2d(k.center))
 	}
 	cutouts := Union2D(s...)
 
@@ -193,21 +183,9 @@ func front_panel() SDF3 {
 
 func main() {
 	s0 := front_panel()
-	s0 = Transform3D(s0, Translate3d(V3{0, 80, 0}))
 	s1 := base()
+	s0 = Transform3D(s0, Translate3d(V3{0, 80, 0}))
 	RenderSTL(Union3D(s0, s1), 400, "x.stl")
-
-	//RenderSTL(front_panel(), 400, "front.stl")
-	//RenderSTL(base(), 400, "base.stl")
-
-	fb := &FingerButtonParms{
-		Width:  3.5,
-		Gap:    0.5,
-		Length: 30.0,
-	}
-
-	RenderDXF(FingerButton2D(fb), 400, "b.dxf")
-
 }
 
 //-----------------------------------------------------------------------------
