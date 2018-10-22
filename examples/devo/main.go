15ca5297d10454e5b6dc2f4e4c150c3791f57a9e
Jason Harris <sirmanlypowers@gmail.com>
2018-10-22 08:31:13 -0700

    Changes to make the energy dome self-supporting during print.

--- examples/devo/main.go
+++ examples/devo/main.go
@@ -16,8 +16,9 @@ import (
 
 //-----------------------------------------------------------------------------
 
-func shell(r, h float64) SDF3 {
+func dome(r, h, w float64) SDF3 {
 
+	// step heights
 	k := 0.8
 	stepH0 := h
 	stepH1 := stepH0 * k
@@ -27,30 +28,56 @@ func shell(r, h float64) SDF3 {
 	height := stepH0 + stepH1 + stepH2 + stepH3
 	fmt.Printf("height %f inches\n", height/MM_PER_INCH)
 
+	// step ledges
 	stepX := (r / 4.0) * 0.75
 	stepX0 := stepX * 0.20
 	stepX1 := stepX - stepX0
 
-	fillet := 4.0
-
+	// outer shell
 	p := NewPolygon()
-
 	p.Add(0, 0)
 	p.Add(r, 0).Rel()
-
-	p.Add(-stepX0, stepH0).Rel().Smooth(fillet, 4)
-	p.Add(-stepX1, 0).Rel().Smooth(fillet, 4)
-
-	p.Add(-stepX0, stepH1).Rel().Smooth(fillet, 4)
-	p.Add(-stepX1, 0).Rel().Smooth(fillet, 4)
-
-	p.Add(-stepX0, stepH2).Rel().Smooth(fillet, 4)
-	p.Add(-stepX1, 0).Rel().Smooth(fillet, 4)
-
-	p.Add(-stepX0, stepH3).Rel().Smooth(fillet, 4)
+	p.Add(-stepX0, stepH0).Rel()
+	p.Add(-stepX1, 0).Rel()
+	p.Add(-stepX0, stepH1).Rel()
+	p.Add(-stepX1, 0).Rel()
+	p.Add(-stepX0, stepH2).Rel()
+	p.Add(-stepX1, 0).Rel()
+	p.Add(-stepX0, stepH3).Rel()
 	p.Add(0, height)
+	outer := Revolve3D(Polygon2D(p.Vertices()))
 
-	return Revolve3D(Polygon2D(p.Vertices()))
+	// inner shell
+
+	b := NewBezier()
+
+	x := 0.0
+	y := 0.0
+	b.Add(x, y)
+
+	x += r - w
+	b.Add(x, y)
+
+	x -= stepX
+	y += stepH0 - w
+	b.Add(x, y)
+
+	x -= stepX
+	y += stepH1
+	b.Add(x, y)
+
+	x -= stepX
+	y += stepH2
+	b.Add(x, y)
+
+	y += stepH3
+	b.Add(0, y)
+
+	b.Close()
+
+	inner := Revolve3D(Polygon2D(b.Polygon().Vertices()))
+
+	return Difference3D(outer, inner)
 }
 
 //-----------------------------------------------------------------------------
@@ -60,12 +87,9 @@ func main() {
 	h0 := 2.05 * MM_PER_INCH
 	wall := 4.0
 
-	outer := shell(radius, h0)
-
-	inner := shell(radius-wall, h0)
-	inner = Transform3D(inner, Translate3d(V3{0, 0, -wall}))
-
-	RenderSTL(Difference3D(outer, inner), 150, "energy_dome.stl")
+	s := dome(radius, h0, wall)
+	//s = Cut3D(s, V3{0, 0, 0}, V3{0, 1, 0})
+	RenderSTL(s, 150, "energy_dome.stl")
 }
 
 //-----------------------------------------------------------------------------
