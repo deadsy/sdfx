f1f71a73588cccbc9c3a28d30f20d5d23c407465
Jason Harris <sirmanlypowers@gmail.com>
2020-11-05 10:27:27 -0800

    Midget engine. More crankcase work.

--- examples/midget/crankcase.go
+++ examples/midget/crankcase.go
@@ -22,9 +22,48 @@ const crankcaseOuterHeight = 7.0 / 8.0
 const crankcaseInnerHeight = 5.0 / 8.0
 const boltLugRadius = 0.5 * (7.0 / 16.0)
 
+const mountLength = 4.75
+const mountWidth = 4.75
+const mountThickness = 0.25
+
 //-----------------------------------------------------------------------------
 
-func crankCaseBoltLugs() sdf.SDF3 {
+// mountLugs returns the lugs used to mount the motor.
+func mountLugs() sdf.SDF3 {
+	const draft = 3.0
+	const thickness = 0.25
+
+	k := sdf.TruncRectPyramidParms{
+		Size:        sdf.V3{4.75, thickness, crankcaseOuterHeight},
+		BaseAngle:   sdf.DtoR(90 - draft),
+		BaseRadius:  crankcaseOuterHeight * 0.1,
+		RoundRadius: crankcaseOuterHeight * 0.1,
+	}
+
+	s := sdf.TruncRectPyramid3D(&k)
+	return sdf.Transform3D(s, sdf.Translate3d(sdf.V3{0, thickness * 0.5, 0}))
+}
+
+//-----------------------------------------------------------------------------
+
+func cylinderMount() sdf.SDF3 {
+	const draft = 3.0
+
+	k := sdf.TruncRectPyramidParms{
+		Size:        sdf.V3{2.0, 5.0 / 16.0, 1 + (3.0 / 16.0)},
+		BaseAngle:   sdf.DtoR(90 - draft),
+		BaseRadius:  crankcaseOuterHeight * 0.1,
+		RoundRadius: crankcaseOuterHeight * 0.1,
+	}
+
+	s := sdf.TruncRectPyramid3D(&k)
+	return sdf.Transform3D(s, sdf.Translate3d(sdf.V3{0, crankcaseInnerRadius, 0}))
+}
+
+//-----------------------------------------------------------------------------
+
+// boltLugs returns lugs that hold the crankcase halves together.
+func boltLugs() sdf.SDF3 {
 
 	const draft = 3.0
 
@@ -53,7 +92,7 @@ func crankCaseBoltLugs() sdf.SDF3 {
 
 //-----------------------------------------------------------------------------
 
-func crankCaseFrontPattern() sdf.SDF3 {
+func basePattern() sdf.SDF3 {
 
 	const draft = 3.0
 
@@ -65,15 +104,33 @@ func crankCaseFrontPattern() sdf.SDF3 {
 	}
 
 	body := sdf.TruncRectPyramid3D(&k)
-	lugs := crankCaseBoltLugs()
 
-	// add the lugs to the body with filleting
-	s := sdf.Union3D(body, lugs)
+	// add the bolt/mount lugs to the body with filleting
+	s := sdf.Union3D(body, boltLugs(), mountLugs())
 	s.(*sdf.UnionSDF3).SetMin(sdf.PolyMin(0.1))
 
-	// cleanup the top/bottom artifacts caused by the filleting
-	s = sdf.Cut3D(s, sdf.V3{0, 0, 0}, sdf.V3{0, 0, 1})
+	// cleanup the top artifacts caused by the filleting
 	s = sdf.Cut3D(s, sdf.V3{0, 0, crankcaseOuterHeight}, sdf.V3{0, 0, -1})
+
+	// add the cylinder mount
+	s = sdf.Union3D(s, cylinderMount())
+	s.(*sdf.UnionSDF3).SetMin(sdf.PolyMin(0.1))
+
+	// cleanup the bottom artifacts caused by the filleting
+	s = sdf.Cut3D(s, sdf.V3{0, 0, 0}, sdf.V3{0, 0, 1})
+
+	return s
+}
+
+//-----------------------------------------------------------------------------
+
+func ccRearPattern() sdf.SDF3 {
+	s := basePattern()
+	return s
+}
+
+func ccFrontPattern() sdf.SDF3 {
+	s := basePattern()
 	return s
 }
 
--- examples/midget/main.go
+++ examples/midget/main.go
@@ -24,7 +24,7 @@ const shrink = 1.0 / 0.999 // PLA ~0.1%
 func main() {
 	scale := shrink * sdf.MillimetresPerInch
 	sdf.RenderSTL(sdf.ScaleUniform3D(cylinderPattern(true, true), scale), 330, "cylinder_pattern.stl")
-	sdf.RenderSTL(sdf.ScaleUniform3D(crankCaseFrontPattern(), scale), 300, "crankcase_front.stl")
+	sdf.RenderSTL(sdf.ScaleUniform3D(ccFrontPattern(), scale), 300, "crankcase_front.stl")
 
 	//sdf.RenderSTL(sdf.ScaleUniform3D(cylinderCoreBox(), shrink), 330, "cylinder_corebox.stl")
 }
