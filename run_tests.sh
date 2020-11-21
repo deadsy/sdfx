e0cea1bc876d78173db07b1da57ca90bcedd7f3b
Steve Traugott <stevegt@t7a.org>
2020-11-20 18:42:43 -0800

    add test cases to temporary test script

--- run_tests.sh
+++ run_tests.sh
@@ -1,11 +1,20 @@
  #!/bin/bash
+
+ # this is a temporary hack to be used until we create more complete
+ # *_test.go files 
+
+ # first run the tests we do have (may want to add coverage flags later)
+ cd sdf
+ go test
+ cd -
  
- nice time for d in examples/*
+ # then run all the examples (this takes a while)
+ time for d in examples/*
  do 
     echo $d
     cd $d
-    make || break
-    ./$(basename $d) || break
+    make || exit 1
+    ./$(basename $d) || exit 1
     make clean
     cd -
  done
