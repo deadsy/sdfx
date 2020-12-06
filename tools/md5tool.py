9046c29cb4c5411843554aadef0afc8927c671c6
Jason Harris <sirmanlypowers@gmail.com>
2020-12-06 11:27:16 -0800

    pylint fixups.

--- tools/md5tool.py
+++ tools/md5tool.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
+"""
+Output md5 hashes for a set of matched files.
+"""
 
 import glob
 import hashlib
 
 def md5sum(filename):
+  """return the md5 hash of a file"""
   with open(filename, mode='rb') as f:
     d = hashlib.md5()
     while True:
@@ -14,8 +18,9 @@ def md5sum(filename):
     return d.hexdigest()
 
 def main():
+  """entry point"""
   f = []
-  for ext in ("stl","svg","dxf","png"):
+  for ext in ("stl", "svg", "dxf", "png"):
     f.extend(glob.glob('*.%s' % ext))
   for fname in f:
     print("%s\t%s" % (md5sum(fname), fname))
