b7d252fb4173172e74b1df611e26410fcfa92df4
Jason Harris <sirmanlypowers@gmail.com>
2020-12-09 19:04:40 -0800

    Using shasum rather than md5sum because the linux/mac tool is consistent.

--- mk/example.mk
+++ mk/example.mk
@@ -7,12 +7,12 @@ all:
 .PHONY: test
 test: all
 	./$(EXEC)
-	if [ -f MD5SUM ]; then md5sum -c MD5SUM; fi;
+	if [ -f SHA1SUM ]; then shasum -c SHA1SUM; fi;
 
 .PHONY: hash
 hash: all
 	./$(EXEC)
-	$(TOP)/tools/md5tool.py > MD5SUM
+	$(TOP)/tools/sha1tool.py > SHA1SUM
 
 .PHONY: clean
 clean:
