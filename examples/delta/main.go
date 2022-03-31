d1d029e2485ca66d51a16854f54408ffe0f03356
Jason Harris <sirmanlypowers@gmail.com>
2022-03-30 18:15:09 -0700

    Added a mounting board for the servo controller.

--- examples/delta/main.go
+++ examples/delta/main.go
@@ -158,6 +158,44 @@ func servoMount() (sdf.SDF3, error) {
 
 //-----------------------------------------------------------------------------
 
+func servoControllerMount() (sdf.SDF3, error) {
+
+	// standoff
+	k0 := obj.StandoffParms{
+		PillarHeight:   0.5 * sdf.MillimetresPerInch,
+		PillarDiameter: 5,
+		HoleDepth:      10,
+		HoleDiameter:   2.4, // #4 screw
+	}
+	standoff, err := obj.Standoff3D(&k0)
+	if err != nil {
+		return nil, err
+	}
+
+	// standoffs
+	h0 := sdf.V3{-0.45, -0.8, 0.25}.MulScalar(sdf.MillimetresPerInch)
+	h1 := sdf.V3{0.05, 0.8, 0.25}.MulScalar(sdf.MillimetresPerInch)
+	standoffs := sdf.Multi3D(standoff, []sdf.V3{h0, h1})
+
+	// base
+	k1 := obj.PanelParms{
+		Size:         sdf.V2{1.1, 1.8}.MulScalar(sdf.MillimetresPerInch),
+		CornerRadius: 2,
+		HoleDiameter: 2.4, // #4 screw
+		HoleMargin:   [4]float64{4, 4, 4, 4},
+		HolePattern:  [4]string{"x", "x", ".x", ""},
+		Thickness:    3,
+	}
+	base, err := obj.Panel3D(&k1)
+	if err != nil {
+		return nil, err
+	}
+
+	return sdf.Union3D(base, standoffs), nil
+}
+
+//-----------------------------------------------------------------------------
+
 func main() {
 
 	s, err := upperArm()
@@ -173,6 +211,13 @@ func main() {
 	}
 	s = sdf.ScaleUniform3D(s, shrink)
 	render.ToSTL(s, 300, "servomount.stl", &render.MarchingCubesOctree{})
+
+	s, err = servoControllerMount()
+	if err != nil {
+		log.Fatalf("error: %s", err)
+	}
+	s = sdf.ScaleUniform3D(s, shrink)
+	render.ToSTL(s, 300, "ctrlmount.stl", &render.MarchingCubesOctree{})
 
 }
 
