ed067b37ddc34021911876432efe5bd22c3bf1bb
Megidd Git <megiddgit@gmail.com>
2022-10-27 15:55:23 +0330

    Carve out the inside of the input model

--- examples/hollowing_stl/main.go
+++ examples/hollowing_stl/main.go
@@ -1,9 +1,7 @@
 //-----------------------------------------------------------------------------
 /*
 
-Build a box using offsets from a rectangular box.
-
-TODO Add a retaining lip to the base or top so the lid stays in place.
+Import an existing STL. Carve its inside before hollowing it. Re-render.
 
 */
 //-----------------------------------------------------------------------------
@@ -12,69 +10,47 @@ package main
 
 import (
 	"log"
+	"os"
 
+	"github.com/deadsy/sdfx/obj"
 	"github.com/deadsy/sdfx/render"
 	"github.com/deadsy/sdfx/sdf"
-	v3 "github.com/deadsy/sdfx/vec/v3"
 )
 
 //-----------------------------------------------------------------------------
 
-const sizeX = 30.0
-const sizeY = 40.0
-const sizeZ = 30.0
-
-const wallThickness = 3.0
-const outerRadius = 6.0
-const lidPosition = 0.75 // 0..1 position of lid on box
+const wallThickness = 1.0
 
 //-----------------------------------------------------------------------------
 
-func box() error {
+func carveinside(stl string) (sdf.SDF3, error) {
 
-	if outerRadius < wallThickness {
-		return sdf.ErrMsg("outerRadius < wallThickness")
-	}
-
-	innerOfs := outerRadius - wallThickness
-	outerOfs := innerOfs + wallThickness
-
-	if sizeX < outerOfs {
-		return sdf.ErrMsg("sizeX < outerOfs")
-	}
-	if sizeY < outerOfs {
-		return sdf.ErrMsg("sizeY < outerOfs")
-	}
-	if sizeZ < outerOfs {
-		return sdf.ErrMsg("sizeZ < outerOfs")
-	}
-
-	baseBox, err := sdf.Box3D(v3.Vec{sizeX - outerOfs, sizeY - outerOfs, sizeZ - outerOfs}, 0)
+	// read the stl file.
+	file, err := os.OpenFile(stl, os.O_RDONLY, 0400)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	innerBox := sdf.Offset3D(baseBox, innerOfs)
-	outerBox := sdf.Offset3D(baseBox, outerOfs)
-	box := sdf.Difference3D(outerBox, innerBox)
+	// create the SDF from the mesh
+	// WARNING: It will only work on non-intersecting closed-surface(s) meshes.
+	imported, err := obj.ImportSTL(file, 20, 3, 5)
+	if err != nil {
+		return nil, err
+	}
 
-	lidZ := (lidPosition - 0.5) * sizeZ
-	base := sdf.Cut3D(box, v3.Vec{0, 0, lidZ}, v3.Vec{0, 0, -1})
-	top := sdf.Cut3D(box, v3.Vec{0, 0, lidZ}, v3.Vec{0, 0, 1})
+	inside := sdf.Offset3D(imported, -wallThickness) // Pass negative value for inside.
 
-	render.ToSTL(base, "base.stl", render.NewMarchingCubesOctree(300))
-	render.ToSTL(top, "top.stl", render.NewMarchingCubesOctree(300))
-
-	return nil
+	return inside, nil
 }
 
 //-----------------------------------------------------------------------------
 
 func main() {
-	err := box()
+	inside, err := carveinside("Utah_teapot_solid.stl")
 	if err != nil {
 		log.Fatalf("error: %s", err)
 	}
+	render.ToSTL(inside, "inside-carved-out.stl", render.NewMarchingCubesOctree(300))
 }
 
 //-----------------------------------------------------------------------------
