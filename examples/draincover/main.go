617673f472900745a81ff7021e2c061694d795b6
Jason Harris <sirmanlypowers@gmail.com>
2023-02-02 11:03:27 -0800

    Added grate holes to the drain cover design.

--- examples/draincover/main.go
+++ examples/draincover/main.go
@@ -18,19 +18,36 @@ import (
 
 //-----------------------------------------------------------------------------
 
+func drain4() (sdf.SDF3, error) {
+	k := &obj.DrainCoverParms{
+		WallDiameter:   3.9 * sdf.MillimetresPerInch,
+		WallHeight:     0.8 * sdf.MillimetresPerInch,
+		WallThickness:  0.2 * sdf.MillimetresPerInch,
+		WallDraft:      sdf.DtoR(2.0),
+		OuterWidth:     0.4 * sdf.MillimetresPerInch,
+		InnerWidth:     0.3 * sdf.MillimetresPerInch,
+		CoverThickness: 0.2 * sdf.MillimetresPerInch,
+		GrateNumber:    8,
+		GrateWidth:     1.1,
+		CrossBarWidth:  0.8,
+		GrateDraft:     sdf.DtoR(8.0),
+	}
+	return obj.DrainCover(k)
+}
+
 func drain12() (sdf.SDF3, error) {
 	k := &obj.DrainCoverParms{
-		WallDiameter:   11.75 * sdf.MillimetresPerInch,
+		WallDiameter:   11.8 * sdf.MillimetresPerInch,
 		WallHeight:     1.0 * sdf.MillimetresPerInch,
-		WallThickness:  0.25 * sdf.MillimetresPerInch,
+		WallThickness:  0.3 * sdf.MillimetresPerInch,
 		WallDraft:      sdf.DtoR(2.0),
-		OuterWidth:     0.75 * sdf.MillimetresPerInch,
+		OuterWidth:     0.8 * sdf.MillimetresPerInch,
 		InnerWidth:     0.5 * sdf.MillimetresPerInch,
-		CoverThickness: 0.25 * sdf.MillimetresPerInch,
+		CoverThickness: 0.3 * sdf.MillimetresPerInch,
 		GrateNumber:    10,
-		GrateWidth:     0.6,
-		CrossBarWidth:  1.0 * sdf.MillimetresPerInch,
-		GrateDraft:     sdf.DtoR(10.0),
+		GrateWidth:     1.0,
+		CrossBarWidth:  1.5,
+		GrateDraft:     sdf.DtoR(8.0),
 	}
 	return obj.DrainCover(k)
 }
@@ -38,11 +55,17 @@ func drain12() (sdf.SDF3, error) {
 //-----------------------------------------------------------------------------
 
 func main() {
-	s, err := drain12()
+	s, err := drain4()
 	if err != nil {
 		log.Fatalf("error: %s", err)
 	}
-	render.ToSTL(s, "drain12.stl", render.NewMarchingCubesOctree(300))
+	render.ToSTL(s, "drain4.stl", render.NewMarchingCubesOctree(300))
+
+	s, err = drain12()
+	if err != nil {
+		log.Fatalf("error: %s", err)
+	}
+	render.ToSTL(s, "drain12.stl", render.NewMarchingCubesOctree(400))
 }
 
 //-----------------------------------------------------------------------------
--- obj/draincover.go
+++ obj/draincover.go
@@ -3,6 +3,9 @@
 
 Drain Covers
 
+This code implements a parametric drain cover. Draft angles are used so a
+3d printed cover can be used as a sand casting pattern.
+
 */
 //-----------------------------------------------------------------------------
 
@@ -12,6 +15,7 @@ import (
 	"math"
 
 	"github.com/deadsy/sdfx/sdf"
+	v3 "github.com/deadsy/sdfx/vec/v3"
 )
 
 //-----------------------------------------------------------------------------
@@ -26,14 +30,14 @@ type DrainCoverParms struct {
 	InnerWidth     float64 // width between inner wall and grate field
 	CoverThickness float64 // thickness of the drain cover
 	GrateNumber    int     // number of grate holes
-	GrateWidth     float64 // width of the grate hole (0..1)
-	CrossBarWidth  float64
-	GrateDraft     float64 // draft anfle of grates (radians)
+	GrateWidth     float64 // width of the grate hole
+	CrossBarWidth  float64 // multiple of InnerWidth
+	GrateDraft     float64 // draft angle of grates (radians)
 }
 
 //-----------------------------------------------------------------------------
 
-func drainCoverBody(k *DrainCoverParms) (sdf.SDF3, error) {
+func dcBody(k *DrainCoverParms) (sdf.SDF3, error) {
 
 	// x drafts
 	dx0 := 0.5 * k.CoverThickness * math.Tan(k.WallDraft)
@@ -52,11 +56,11 @@ func drainCoverBody(k *DrainCoverParms) (sdf.SDF3, error) {
 	p := sdf.NewPolygon()
 	p.Add(0, 0)
 	p.Add(r1+dx0, 0)
-	p.Add(r1-dx0, t0)
-	p.Add(r0+dx1, t0)
-	p.Add(r0-dx1, t1)
-	p.Add(r2+dx1, t1)
-	p.Add(r2-dx1, t0)
+	p.Add(r1-dx0, t0).Smooth(0.25*k.CoverThickness, 4)
+	p.Add(r0+dx1, t0).Smooth(0.25*k.WallThickness, 4)
+	p.Add(r0-dx1, t1).Smooth(0.25*k.WallThickness, 4)
+	p.Add(r2+dx1, t1).Smooth(0.25*k.WallThickness, 4)
+	p.Add(r2-dx1, t0).Smooth(0.25*k.WallThickness, 4)
 	p.Add(0, t0)
 
 	s, err := sdf.Polygon2D(p.Vertices())
@@ -68,9 +72,91 @@ func drainCoverBody(k *DrainCoverParms) (sdf.SDF3, error) {
 	return sdf.Revolve3D(s)
 }
 
+// dcGrate returns a grate (no crossbar)
+func dcGrate(k *DrainCoverParms) (sdf.SDF3, error) {
+
+	r := (0.5 * k.WallDiameter) - k.InnerWidth
+	n := float64(k.GrateNumber)
+	g := (2.0 * r) / (n + (n * k.GrateWidth) + 1.0)
+	w := k.GrateWidth * g
+
+	x := g + (0.5 * w) - r
+	dx := g + w
+
+	slots := make([]sdf.SDF3, k.GrateNumber)
+	for i := 0; i < k.GrateNumber; i++ {
+		l := 2.0 * math.Sqrt((r*r)-(x*x))
+		k1 := TruncRectPyramidParms{
+			Size:       v3.Vec{w, l, k.CoverThickness},
+			BaseAngle:  0.5*sdf.Pi - k.GrateDraft,
+			BaseRadius: 0.5 * w,
+		}
+		slot, err := TruncRectPyramid3D(&k1)
+		if err != nil {
+			return nil, err
+		}
+		slots[i] = sdf.Transform3D(slot, sdf.Translate3d(v3.Vec{x, 0, -k.CoverThickness}))
+		x += dx
+	}
+
+	return sdf.Transform3D(sdf.Union3D(slots...), sdf.MirrorXY()), nil
+}
+
+// dcGrate returns a grate with a crossbar
+func dcGrateCrossBar(k *DrainCoverParms) (sdf.SDF3, error) {
+
+	r := (0.5 * k.WallDiameter) - k.InnerWidth
+	n := float64(k.GrateNumber)
+	g := (2.0 * r) / (n + (n * k.GrateWidth) + 1.0)
+	w := k.GrateWidth * g
+
+	x := g + (0.5 * w) - r
+	dx := g + w
+	dy := 0.5 * k.InnerWidth * k.CrossBarWidth
+
+	slots := make([]sdf.SDF3, k.GrateNumber)
+	for i := 0; i < k.GrateNumber; i++ {
+		l := math.Sqrt((r*r)-(x*x)) - dy
+		y := dy + (0.5 * l)
+
+		k1 := TruncRectPyramidParms{
+			Size:       v3.Vec{w, l, k.CoverThickness},
+			BaseAngle:  0.5*sdf.Pi - k.GrateDraft,
+			BaseRadius: 0.5 * w,
+		}
+		slot, err := TruncRectPyramid3D(&k1)
+		if err != nil {
+			return nil, err
+		}
+		slots[i] = sdf.Transform3D(slot, sdf.Translate3d(v3.Vec{x, y, -k.CoverThickness}))
+		x += dx
+	}
+
+	g0 := sdf.Transform3D(sdf.Union3D(slots...), sdf.MirrorXY())
+	g1 := sdf.Transform3D(g0, sdf.MirrorXZ())
+
+	return sdf.Union3D(g0, g1), nil
+}
+
 // DrainCover returns a grated drain pipe cover.
 func DrainCover(k *DrainCoverParms) (sdf.SDF3, error) {
-	return drainCoverBody(k)
+
+	body, err := dcBody(k)
+	if err != nil {
+		return nil, err
+	}
+
+	var grate sdf.SDF3
+	if k.CrossBarWidth == 0 {
+		grate, err = dcGrate(k)
+	} else {
+		grate, err = dcGrateCrossBar(k)
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	return sdf.Difference3D(body, grate), nil
 }
 
 //-----------------------------------------------------------------------------
