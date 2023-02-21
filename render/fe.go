4096f91f5f9c965ffbf1f5feab514f7f0f8d7360
Megidd Git <megiddgit@gmail.com>
2023-02-21 11:39:11 +0330

    Comment

--- render/fe.go
+++ render/fe.go
@@ -3,6 +3,7 @@ package render
 import v3 "github.com/deadsy/sdfx/vec/v3"
 
 // Tetrahedron is a 3D tetrahedron.
+// It's a type of finite element, FE.
 // https://en.wikipedia.org/wiki/Tetrahedron
 type Tetrahedron struct {
 	V [4]v3.Vec
