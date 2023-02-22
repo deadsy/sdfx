ac20caaa98796f3f9588eb8d8d5dfddf6089f3c2
Megidd Git <megiddgit@gmail.com>
2023-02-22 14:56:55 +0330

    Write to output file: comments and heading

--- render/fewrite.go
+++ render/fewrite.go
@@ -1,13 +1,63 @@
 package render
 
 import (
+	"bufio"
+	"encoding/binary"
 	"os"
 	"sync"
+	"time"
 )
+
+//-----------------------------------------------------------------------------
+
+// Define the ABAQUS or CalculiX inp file sections.
+
+type InpComments struct {
+	Text string // General comments.
+}
+
+type InpHeading struct {
+	Title  string //
+	Break0 string //
+	Model  string //
+	Tab    string //
+	Date   string //
+	Break1 string //
+}
+
+//-----------------------------------------------------------------------------
 
 // writeFE writes a stream of finite elements in the shape of tetrahedra to an ABAQUS or CalculiX file.
 func writeFE(wg *sync.WaitGroup, path string) (chan<- []*Tetrahedron, error) {
 	f, err := os.Create(path)
+	if err != nil {
+		return nil, err
+	}
+
+	// Use buffered IO for optimal IO writes.
+	// The default buffer size doesn't appear to limit performance.
+	buf := bufio.NewWriter(f)
+
+	// write general comments
+	cmnts := InpComments{
+		Text: `**
+		** Structure: tetrahedral elements of a 3D model.
+		**\n`,
+	}
+	err = binary.Write(buf, binary.LittleEndian, &cmnts)
+	if err != nil {
+		return nil, err
+	}
+
+	hdng := InpHeading{
+		Title:  "*HEADING",
+		Break0: "\n",
+		Model:  "Model: 3D model",
+		Tab:    "\t",
+		Date:   "Date: " + time.Now().UTC().Format("2006-January-02 MST"),
+		Break1: "\n",
+	}
+	err = binary.Write(buf, binary.LittleEndian, &hdng)
 	if err != nil {
 		return nil, err
 	}
@@ -28,6 +78,9 @@ func writeFE(wg *sync.WaitGroup, path string) (chan<- []*Tetrahedron, error) {
 				// TODO.
 			}
 		}
+
+		// flush the tetrahedra
+		buf.Flush()
 	}()
 
 	return c, nil
