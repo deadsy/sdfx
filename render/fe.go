8f19dfd9ed732c3273e59cfea49be199338c0c33
Megidd Git <megiddgit@gmail.com>
2023-02-25 13:49:44 +0330

    Tet4 mesh funcs

--- render/fe.go
+++ render/fe.go
@@ -16,5 +16,48 @@ type Tet4 struct {
 type MeshTet4 struct {
 	T      []uint32              // Index buffer. Every 4 indices would correspond to a tetrahedron.
 	V      []v3.Vec              // Vertex buffer. All unique.
-	Lookup map[[4]float32]uint32 // Used to avoid repeating vertices when adding a new tetrahedron.
+	Lookup map[[3]float32]uint32 // Used to avoid repeating vertices when adding a new tetrahedron.
+	vCount uint32                // TODO: Remove?
+}
+
+func NewMeshTet4() *MeshTet4 {
+	return &MeshTet4{
+		T:      []uint32{},
+		V:      []v3.Vec{},
+		Lookup: map[[3]float32]uint32{},
+	}
+}
+
+// Just an optimization, if there is an estimation of tetrahedra count.
+// Affects the speed according to experiments.
+func (m *MeshTet4) Allocate(tetCount uint32) {
+	m.T = make([]uint32, tetCount*4)
+	m.V = make([]v3.Vec, tetCount/4*2) // By experimenting.
+	m.Lookup = make(map[[3]float32]uint32, tetCount/4*2)
+}
+
+func (t *MeshTet4) AddTet(i uint32, a, b, c, d v3.Vec) {
+	// The node numbering should follow the convention of CalculiX.
+	// http://www.dhondt.de/ccx_2.20.pdf
+	t.T[i*4], t.T[i*4+1], t.T[i*4+2], t.T[i*4+3] = t.AddVertex(a), t.AddVertex(b), t.AddVertex(c), t.AddVertex(d)
+}
+
+func (t *MeshTet4) AddVertex(vert v3.Vec) uint32 {
+	// TODO: Binary insertion sort and search to eliminate extra allocation
+	// TODO: Consider epsilon in comparison and use int (*100) for searching
+	if vertID, ok := t.Lookup[[3]float32{float32(vert.X), float32(vert.Y), float32(vert.Z)}]; ok {
+		return vertID
+	}
+	if t.VertexCount() <= int(t.vCount) {
+		t.V = append(t.V, vert)
+	} else {
+		t.V[t.vCount] = vert
+	}
+	t.Lookup[[3]float32{float32(vert.X), float32(vert.Y), float32(vert.Z)}] = t.vCount
+	t.vCount++
+	return t.vCount - 1
+}
+
+func (m *MeshTet4) VertexCount() int {
+	return len(m.V)
 }
