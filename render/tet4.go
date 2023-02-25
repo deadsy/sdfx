b7f42dd40665ed8a0ef561b78a2344453e931847
Megidd Git <megiddgit@gmail.com>
2023-02-25 14:37:13 +0330

    Simplify adding tet4 to mesh

--- render/tet4.go
+++ render/tet4.go
@@ -21,7 +21,6 @@ type MeshTet4 struct {
 	T      []uint32              // Index buffer. Every 4 indices would correspond to a tetrahedron.
 	V      []v3.Vec              // Vertex buffer. All unique.
 	Lookup map[[3]float64]uint32 // Used to avoid repeating vertices when adding a new tetrahedron.
-	vCount uint32                // TODO: Remove?
 }
 
 func NewMeshTet4() *MeshTet4 {
@@ -32,17 +31,13 @@ func NewMeshTet4() *MeshTet4 {
 	}
 }
 
-func (m *MeshTet4) Allocate(tetCount uint32) {
-	m.T = make([]uint32, tetCount*4)
+func (m *MeshTet4) AddTet(a, b, c, d v3.Vec) {
+	// Index of current tetrahedron being added.
+	i := len(m.T) / 4
 
-	// Just an optimization by experimenting.
-	// Affects the speed according to experiments.
-	m.V = make([]v3.Vec, tetCount/4*2)
+	// Make room for a new tetrahedron.
+	m.T = append(m.T, 0, 0, 0, 0)
 
-	m.Lookup = make(map[[3]float64]uint32, tetCount/4*2)
-}
-
-func (m *MeshTet4) AddTet(i uint32, a, b, c, d v3.Vec) {
 	// The node numbering should follow the convention of CalculiX.
 	// http://www.dhondt.de/ccx_2.20.pdf
 	m.T[i*4], m.T[i*4+1], m.T[i*4+2], m.T[i*4+3] = m.AddVertex(a), m.AddVertex(b), m.AddVertex(c), m.AddVertex(d)
@@ -54,14 +49,12 @@ func (m *MeshTet4) AddVertex(vert v3.Vec) uint32 {
 	if vertID, ok := m.Lookup[[3]float64{vert.X, vert.Y, vert.Z}]; ok {
 		return vertID
 	}
-	if m.VertexCount() <= int(m.vCount) {
-		m.V = append(m.V, vert)
-	} else {
-		m.V[m.vCount] = vert
-	}
-	m.Lookup[[3]float64{vert.X, vert.Y, vert.Z}] = m.vCount
-	m.vCount++
-	return m.vCount - 1
+
+	m.V = append(m.V, vert)
+
+	m.Lookup[[3]float64{vert.X, vert.Y, vert.Z}] = uint32(m.VertexCount())
+
+	return uint32(m.VertexCount())
 }
 
 func (m *MeshTet4) VertexCount() int {
@@ -69,7 +62,6 @@ func (m *MeshTet4) VertexCount() int {
 }
 
 func (t *MeshTet4) Finalize() {
-	t.V = t.V[:t.vCount]
 	// Clear memory.
 	t.Lookup = nil
 	runtime.GC()
