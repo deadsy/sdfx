1bd8b5278a4a898ec9f8914dc4b4a80ef85c62c1
Megidd Git <megiddgit@gmail.com>
2023-02-25 13:30:30 +0330

    Mesh data structure

--- render/fe.go
+++ render/fe.go
@@ -10,5 +10,11 @@ type Tet4 struct {
 }
 
 // A mesh of tetrahedra with 4 nodes.
+// A sophisticated data structure for mesh is required to store tetrahedra.
+// The repeated nodes would be removed.
+// The element connectivity would be created with unique nodes.
 type MeshTet4 struct {
+	T      []uint32              // Index buffer. Every 4 indices would correspond to a tetrahedron.
+	V      []v3.Vec              // Vertex buffer. All unique.
+	Lookup map[[4]float32]uint32 // Used to avoid repeating vertices when adding a new tetrahedron.
 }
