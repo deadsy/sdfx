065e5429655a9bf6dce1ac539b14d5aa36928d6f
Megidd Git <megiddgit@gmail.com>
2023-02-25 17:03:27 +0330

    Rename: clear

--- render/marchtet4.go
+++ render/marchtet4.go
@@ -10,7 +10,7 @@ import (
 
 //-----------------------------------------------------------------------------
 
-func marchingTetrahedra(s sdf.SDF3, box sdf.Box3, step float64) []*Tet4 {
+func marchingTet4(s sdf.SDF3, box sdf.Box3, step float64) []*Tet4 {
 	fmt.Printf("marching tetrahedra, bbox center: %v , step: %v\n", s.BoundingBox().Center(), step)
 	var tetrahedra []*Tet4
 
@@ -42,20 +42,20 @@ func marchingTetrahedra(s sdf.SDF3, box sdf.Box3, step float64) []*Tet4 {
 
 //-----------------------------------------------------------------------------
 
-// MarchingTetrahedraUniform renders using marching Tetrahedra with uniform space sampling.
-type MarchingTetrahedraUniform struct {
+// MarchingTet4Uniform renders using marching Tetrahedra with uniform space sampling.
+type MarchingTet4Uniform struct {
 	meshCells int // number of cells on the longest axis of bounding box. e.g 200
 }
 
-// NewMarchingTetrahedraUniform returns a RenderFE object.
-func NewMarchingTetrahedraUniform(meshCells int) *MarchingTetrahedraUniform {
-	return &MarchingTetrahedraUniform{
+// NewMarchingTet4Uniform returns a RenderFE object.
+func NewMarchingTet4Uniform(meshCells int) *MarchingTet4Uniform {
+	return &MarchingTet4Uniform{
 		meshCells: meshCells,
 	}
 }
 
 // Info returns a string describing the rendered volume.
-func (r *MarchingTetrahedraUniform) Info(s sdf.SDF3) string {
+func (r *MarchingTet4Uniform) Info(s sdf.SDF3) string {
 	bb0 := s.BoundingBox()
 	bb0Size := bb0.Size()
 	meshInc := bb0Size.MaxComponent() / float64(r.meshCells)
@@ -67,7 +67,7 @@ func (r *MarchingTetrahedraUniform) Info(s sdf.SDF3) string {
 
 // Render produces a finite elements mesh over the bounding volume of an sdf3.
 // Finite elements are in the shape of tetrahedra.
-func (r *MarchingTetrahedraUniform) Render(s sdf.SDF3, output chan<- []*Tet4) {
+func (r *MarchingTet4Uniform) Render(s sdf.SDF3, output chan<- []*Tet4) {
 	// work out the region we will sample
 	bb0 := s.BoundingBox()
 	bb0Size := bb0.Size()
@@ -76,7 +76,7 @@ func (r *MarchingTetrahedraUniform) Render(s sdf.SDF3, output chan<- []*Tet4) {
 	bb1Size = bb1Size.Ceil().AddScalar(1)
 	bb1Size = bb1Size.MulScalar(meshInc)
 	bb := sdf.NewBox3(bb0.Center(), bb1Size)
-	output <- marchingTetrahedra(s, bb, meshInc)
+	output <- marchingTet4(s, bb, meshInc)
 }
 
 //-----------------------------------------------------------------------------
