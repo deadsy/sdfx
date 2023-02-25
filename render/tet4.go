0e7fc7405d820d6a65ea6ec297f200ccd7746d82
Megidd Git <megiddgit@gmail.com>
2023-02-25 15:06:52 +0330

    Revert "Low-level vertex buffer"
    This reverts commit ac01b82595df5f7ea0686d86be026d5daadd000c.

--- render/tet4.go
+++ render/tet4.go
@@ -17,37 +17,36 @@ type Tet4 struct {
 // A sophisticated data structure for mesh is required to store tetrahedra.
 // The repeated nodes would be removed.
 // The element connectivity would be created with unique nodes.
-// Keep it low-level to increase performance. Indicated by experiments.
 type MeshTet4 struct {
-	T      []uint32              // Index buffer. Every 4 indices would correspond to a tetrahedron of (i, j, k, l).
-	V      []float32             // Vertex buffer. Every 3 floats would correspond to a vertex of (x, y, z).
-	Lookup map[[3]float32]uint32 // Used to avoid repeating vertices when adding a new tetrahedron.
+	T      []uint32              // Index buffer. Every 4 indices would correspond to a tetrahedron.
+	V      []v3.Vec              // Vertex buffer. All unique.
+	Lookup map[[3]float64]uint32 // Used to avoid repeating vertices when adding a new tetrahedron.
 }
 
 func NewMeshTet4() *MeshTet4 {
 	return &MeshTet4{
 		T:      []uint32{},
-		V:      []float32{},
-		Lookup: map[[3]float32]uint32{},
+		V:      []v3.Vec{},
+		Lookup: map[[3]float64]uint32{},
 	}
 }
 
 // The node numbering should follow the convention of CalculiX.
 // http://www.dhondt.de/ccx_2.20.pdf
-func (m *MeshTet4) AddTet4(a, b, c, d [3]float32) {
+func (m *MeshTet4) AddTet4(a, b, c, d v3.Vec) {
 	m.T = append(m.T, m.addVertex(a), m.addVertex(b), m.addVertex(c), m.addVertex(d))
 }
 
-func (m *MeshTet4) addVertex(vert [3]float32) uint32 {
+func (m *MeshTet4) addVertex(vert v3.Vec) uint32 {
 	// TODO: Binary insertion sort and search to eliminate extra allocation
 	// TODO: Consider epsilon in comparison and use int (*100) for searching
-	if vertID, ok := m.Lookup[vert]; ok {
+	if vertID, ok := m.Lookup[[3]float64{vert.X, vert.Y, vert.Z}]; ok {
 		return vertID
 	}
 
-	m.V = append(m.V, vert[0], vert[1], vert[2])
+	m.V = append(m.V, vert)
 
-	m.Lookup[vert] = uint32(m.countVertex())
+	m.Lookup[[3]float64{vert.X, vert.Y, vert.Z}] = uint32(m.countVertex())
 
 	return uint32(m.countVertex())
 }
@@ -76,9 +75,6 @@ func (m *MeshTet4) Tet4Indicies(i int) (uint32, uint32, uint32, uint32) {
 
 // Input from 0 to number of tetrahedra on mesh.
 // Don't return error to increase performance.
-func (m *MeshTet4) Tet4Vertices(i int) ([3]float32, [3]float32, [3]float32, [3]float32) {
-	return [3]float32{m.V[m.T[i*4]*3], m.V[m.T[i*4]*3+1], m.V[m.T[i*4]*3+2]},
-		[3]float32{m.V[m.T[i*4+1]*3], m.V[m.T[i*4+1]*3+1], m.V[m.T[i*4+1]*3+2]},
-		[3]float32{m.V[m.T[i*4+2]*3], m.V[m.T[i*4+2]*3+1], m.V[m.T[i*4+2]*3+2]},
-		[3]float32{m.V[m.T[i*4+3]*3], m.V[m.T[i*4+3]*3+1], m.V[m.T[i*4+3]*3+2]}
+func (m *MeshTet4) Tet4Vertices(i int) (v3.Vec, v3.Vec, v3.Vec, v3.Vec) {
+	return m.V[m.T[i*4]], m.V[m.T[i*4+1]], m.V[m.T[i*4+2]], m.V[m.T[i*4+3]]
 }
