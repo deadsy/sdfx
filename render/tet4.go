32aa868b37ea8e53877d01c537562406fb2a25f0
Megidd Git <megiddgit@gmail.com>
2023-02-26 13:03:00 +0330

    Fix bug: element ID

--- render/tet4.go
+++ render/tet4.go
@@ -173,15 +173,17 @@ func (m *MeshTet4) WriteInp(path string) error {
 		return err
 	}
 
+	var eleID uint32
 	var idx0, idx1, idx2, idx3 uint32
 	for l := 0; l < m.LayerCount(); l++ {
 		for i := 0; i < m.Tet4CountOnLayer(l); i++ {
 			idx0, idx1, idx2, idx3 = m.Tet4Indicies(l, i)
 			// ID starts from one not zero.
-			_, err = f.WriteString(fmt.Sprintf("%d,%d,%d,%d,%d\n", i+1, idx0+1, idx1+1, idx2+1, idx3+1))
+			_, err = f.WriteString(fmt.Sprintf("%d,%d,%d,%d,%d\n", eleID+1, idx0+1, idx1+1, idx2+1, idx3+1))
 			if err != nil {
 				return err
 			}
+			eleID++
 		}
 	}
 
