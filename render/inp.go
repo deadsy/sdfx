2b57c89ca68b77c2bdb093ae80f84b1da67e5b70
Megidd Git <megiddgit@gmail.com>
2023-03-09 15:47:36 +0330

    Simplify

--- render/inp.go
+++ render/inp.go
@@ -2,27 +2,16 @@ package render
 
 import "github.com/deadsy/sdfx/render/buffer"
 
-type FEType int
-
-const (
-	FETet4 FEType = iota + 1
-	FEHex8
-	FEHex20
-)
-
 // To write different types of finite elements as ABAQUS or CalculiX `inp` file.
 type Inp struct {
-	// To be able to write to `inp` for all types of FE.
-	ElType FEType
 	// To write only required nodes to `inp` file.
 	TempVBuff *buffer.VB
 	// Output `inp` file path.
 	Path string
 }
 
-func NewInp(elType FEType, path string) *Inp {
+func NewInp(path string) *Inp {
 	return &Inp{
-		ElType:    elType,
 		TempVBuff: buffer.NewVB(),
 		Path:      path,
 	}
