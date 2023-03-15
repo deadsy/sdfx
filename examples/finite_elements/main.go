abd01e67230337d2fa1591b7cb1cb81956f7b0b4
Megidd Git <megiddgit@gmail.com>
2023-03-15 13:23:22 +0330

    Call adaptive example

--- examples/finite_elements/main.go
+++ examples/finite_elements/main.go
@@ -100,17 +100,6 @@ func hex8adaptive(s sdf.SDF3, resolution int, pth string) error {
 	lyrStart := 0
 	lyrEnd := 20
 
-	// Write just some layers of mesh to a file.
-	//
-	// Units are mm,N,sec.
-	// Force per area = N/mm2 or MPa
-	// Mass density = Ns2/mm4
-	// Refer to the "Units" chapter of:
-	// http://www.dhondt.de/ccx_2.20.pdf
-	//
-	// Mechanical properties are based on typical SLA resins.
-	//
-	// TODO: Correct resin specifications.
 	err := m.WriteInpLayers(pth, lyrStart, lyrEnd, []int{0, 1, 2}, 1.25e-9, 900, 0.3)
 	if err != nil {
 		return err
@@ -147,6 +136,11 @@ func main() {
 	}
 
 	err = hex20(teapotSdf, 80, "teapot-hex20.inp")
+	if err != nil {
+		log.Fatalf("error: %s", err)
+	}
+
+	err = hex8adaptive(teapotSdf, 80, "teapot-hex20-adaptive.inp")
 	if err != nil {
 		log.Fatalf("error: %s", err)
 	}
