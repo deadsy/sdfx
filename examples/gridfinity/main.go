54129003a564ca88e63a7f3da7f9a310dfaac4d1
Jason Harris <sirmanlypowers@gmail.com>
2023-04-29 10:49:40 -0700

    Added more features to the gridfinity body objects.

--- examples/gridfinity/main.go
+++ examples/gridfinity/main.go
@@ -13,14 +13,8 @@ package main
 import (
 	"github.com/deadsy/sdfx/obj"
 	"github.com/deadsy/sdfx/render"
-	"github.com/deadsy/sdfx/sdf"
+	"github.com/deadsy/sdfx/vec/v3i"
 )
-
-//-----------------------------------------------------------------------------
-// material shrinkage
-
-const shrink = 1.0 / 0.999 // PLA ~0.1%
-//const shrink = 1.0/0.995; // ABS ~0.5%
 
 //-----------------------------------------------------------------------------
 
@@ -31,15 +25,21 @@ func main() {
 		Y: 4,
 	}
 	base := obj.GfBase(&kBase)
-	render.ToSTL(sdf.ScaleUniform3D(base, shrink), "base.stl", render.NewMarchingCubesOctree(300))
+	render.ToSTL(base, "base.stl", render.NewMarchingCubesOctree(300))
 
 	kBody := obj.GfBodyParms{
-		X: 1,
-		Y: 2,
-		Z: 2,
+		Size:  v3i.Vec{1, 1, 3},
+		Hole:  true,
+		Empty: true,
 	}
 	body := obj.GfBody(&kBody)
-	render.ToSTL(sdf.ScaleUniform3D(body, shrink), "body.stl", render.NewMarchingCubesOctree(300))
+	render.ToSTL(body, "body_1x1x3.stl", render.NewMarchingCubesOctree(300))
+
+	kBody = obj.GfBodyParms{
+		Size: v3i.Vec{1, 2, 1},
+	}
+	body = obj.GfBody(&kBody)
+	render.ToSTL(body, "body_1x2x1.stl", render.NewMarchingCubesOctree(300))
 
 }
 
--- obj/gridfinity.go
+++ obj/gridfinity.go
@@ -14,70 +14,106 @@ import (
 	"github.com/deadsy/sdfx/sdf"
 	v2 "github.com/deadsy/sdfx/vec/v2"
 	v3 "github.com/deadsy/sdfx/vec/v3"
+	"github.com/deadsy/sdfx/vec/v3i"
 )
 
 //-----------------------------------------------------------------------------
 
-func gfShape(size v2.Vec, h0, h1, h2, round float64) sdf.SDF3 {
-	// upper
+func gfShape(size v2.Vec, h0, h1, h2, h3, round float64) sdf.SDF3 {
+
+	// upper (h0)
 	k := TruncRectPyramidParms{
-		Size:       v3.Vec{size.X, size.Y, h2},
+		Size:       v3.Vec{size.X, size.Y, h0},
 		BaseAngle:  sdf.DtoR(45),
 		BaseRadius: round,
 	}
 	upper, _ := TruncRectPyramid3D(&k)
 
-	// middle
-	size = size.SubScalar(2.0 * h2)
-	round -= h2
+	// middle (h1)
+	size = size.SubScalar(2.0 * h0)
+	round -= h0
 	m2d := sdf.Box2D(size, round)
 	middle := sdf.Extrude3D(m2d, h1)
-	middle = sdf.Transform3D(middle, sdf.Translate3d(v3.Vec{0, 0, h2 + 0.5*h1}))
+	middle = sdf.Transform3D(middle, sdf.Translate3d(v3.Vec{0, 0, h0 + 0.5*h1}))
 
-	//lower
+	// lower (h2)
 	k = TruncRectPyramidParms{
-		Size:       v3.Vec{size.X, size.Y, h0},
+		Size:       v3.Vec{size.X, size.Y, h2},
 		BaseAngle:  sdf.DtoR(45),
 		BaseRadius: round,
 	}
 	lower, _ := TruncRectPyramid3D(&k)
-	lower = sdf.Transform3D(lower, sdf.Translate3d(v3.Vec{0, 0, h2 + h1}))
+	lower = sdf.Transform3D(lower, sdf.Translate3d(v3.Vec{0, 0, h0 + h1}))
 
-	s := sdf.Union3D(upper, middle, lower)
-	return sdf.Transform3D(s, sdf.RotateX(sdf.Pi))
+	// extension (h3)
+	var ext sdf.SDF3
+	if h3 > 0 {
+		size = size.SubScalar(2.0 * h2)
+		round -= h2
+		ext2d := sdf.Box2D(size, round)
+		ext = sdf.Extrude3D(ext2d, h3)
+		ext = sdf.Transform3D(ext, sdf.Translate3d(v3.Vec{0, 0, h0 + h1 + h2 + 0.5*h3}))
+	}
+
+	return sdf.Transform3D(sdf.Union3D(upper, middle, lower, ext), sdf.RotateX(sdf.Pi))
 }
 
 //-----------------------------------------------------------------------------
 
+const gfHoleOffset = 4.8
+const gfHoleMinor = 0.5 * 3.0
+const gfHoleMajor = 0.5 * 6.5
+const gfHoleHeight = 2.0
+
+func gfHoles(r, h, zOfs float64) sdf.SDF3 {
+	const ofs = 0.5*gfMaleSize - (gfMaleH0 + gfMaleH2 + gfHoleOffset)
+	hole, _ := sdf.Cylinder3D(h, r, 0)
+	posn := []v3.Vec{
+		v3.Vec{ofs, ofs, zOfs},
+		v3.Vec{-ofs, ofs, zOfs},
+		v3.Vec{ofs, -ofs, zOfs},
+		v3.Vec{-ofs, -ofs, zOfs},
+	}
+	return sdf.Multi3D(hole, posn)
+}
+
+func gfThruHoles(h float64) sdf.SDF3 {
+	zOfs := 0.5*h - gfMaleHeight + gfHoleHeight
+	return gfHoles(gfHoleMinor, h, zOfs)
+}
+
 const gfFemaleSize = 42.0
-const gfFemaleRound = 8.0 * 0.5
-const gfFemaleH0 = 0.7
+const gfFemaleRound = 0.5 * 8.0
+const gfFemaleH0 = 2.15
 const gfFemaleH1 = 1.8
-const gfFemaleH2 = 2.15
+const gfFemaleH2 = 0.7
+const gfFemaleHeight = gfFemaleH0 + gfFemaleH1 + gfFemaleH2
 
 func gfFemale() sdf.SDF3 {
-	return gfShape(v2.Vec{gfFemaleSize, gfFemaleSize}, gfFemaleH0, gfFemaleH1, gfFemaleH2, gfFemaleRound)
+	return gfShape(v2.Vec{gfFemaleSize, gfFemaleSize}, gfFemaleH0, gfFemaleH1, gfFemaleH2, 0, gfFemaleRound)
 }
 
 const gfMaleSize = 41.5
-const gfMaleRound = 7.5 * 0.5
-const gfMaleH0 = 0.8
+const gfMaleRound = 0.5 * 7.5
+const gfMaleH0 = 2.15
 const gfMaleH1 = 1.8
-const gfMaleH2 = 2.15
+const gfMaleH2 = 0.8
 const gfMaleHeight = gfMaleH0 + gfMaleH1 + gfMaleH2
 
 func gfMale() sdf.SDF3 {
-	return gfShape(v2.Vec{gfMaleSize, gfMaleSize}, gfMaleH0, gfMaleH1, gfMaleH2, gfMaleRound)
+	plug := gfShape(v2.Vec{gfMaleSize, gfMaleSize}, gfMaleH0, gfMaleH1, gfMaleH2, 0, gfMaleRound)
+	holes := gfHoles(gfHoleMajor, gfHoleHeight, 0.5*gfHoleHeight-gfMaleHeight)
+	return sdf.Difference3D(plug, holes)
 }
 
-const gfLipRound = 7.5 * 0.5
-const gfLipH0 = 0.7
+const gfLipRound = 0.5 * 7.5
+const gfLipH0 = 1.9
 const gfLipH1 = 1.8
-const gfLipH2 = 1.9
+const gfLipH2 = 0.7
 const gfLipHeight = gfLipH0 + gfLipH1 + gfLipH2
 
-func gfLip(x, y float64) sdf.SDF3 {
-	return gfShape(v2.Vec{x, y}, gfLipH0, gfLipH1, gfLipH2, gfLipRound)
+func gfLip(x, y, empty float64) sdf.SDF3 {
+	return gfShape(v2.Vec{x, y}, gfLipH0, gfLipH1, gfLipH2, empty, gfLipRound)
 }
 
 //-----------------------------------------------------------------------------
@@ -96,9 +132,8 @@ func GfBase(k *GfBaseParms) sdf.SDF3 {
 	if k.Y <= 0 {
 		k.Y = 1
 	}
-	const h = gfFemaleH0 + gfFemaleH1 + gfFemaleH2
-	if k.Height < h {
-		k.Height = h
+	if k.Height < gfFemaleHeight {
+		k.Height = gfFemaleHeight
 	}
 
 	// base body
@@ -127,49 +162,64 @@ func GfBase(k *GfBaseParms) sdf.SDF3 {
 
 // GfBodyParms are the gridfinity body parameters.
 type GfBodyParms struct {
-	X, Y, Z int // size of body in gridfinity units
+	Size  v3i.Vec // size of body in gridfinity units
+	Empty bool    // return an empty container
+	Hole  bool    // add through holes to the body
 }
 
 const gfHeightSize = 7.0
+const gfFloor = 1.0 // floor thickness for an empty container
 
 // GfBody returns a gridfinity body.
 func GfBody(k *GfBodyParms) sdf.SDF3 {
 
-	if k.X <= 0 {
-		k.X = 1
+	if k.Size.X <= 0 {
+		k.Size.X = 1
 	}
-	if k.Y <= 0 {
-		k.Y = 1
+	if k.Size.Y <= 0 {
+		k.Size.Y = 1
 	}
-	if k.Z <= 0 {
-		k.Z = 1
+	if k.Size.Z <= 0 {
+		k.Size.Z = 1
 	}
 
 	// body
-	size := v2.Vec{float64(k.X), float64(k.Y)}.MulScalar(gfFemaleSize).SubScalar(gfFemaleSize - gfMaleSize)
+	size := v2.Vec{float64(k.Size.X), float64(k.Size.Y)}.MulScalar(gfFemaleSize).SubScalar(gfFemaleSize - gfMaleSize)
 	b2d := sdf.Box2D(size, gfMaleRound)
-	h := (float64(k.Z) * gfHeightSize) + gfLipH0 - gfMaleHeight
+	h := (float64(k.Size.Z) * gfHeightSize) + gfLipHeight - gfMaleHeight
 	body := sdf.Extrude3D(b2d, h)
 
-	// base plugs
-	posn := make([]v3.Vec, k.X*k.Y)
-	xOfs := -0.5 * float64(k.X-1) * gfFemaleSize
-	yOfs := -0.5 * float64(k.Y-1) * gfFemaleSize
+	// grid positions
+	posn := make([]v3.Vec, k.Size.X*k.Size.Y)
+	xOfs := -0.5 * float64(k.Size.X-1) * gfFemaleSize
+	yOfs := -0.5 * float64(k.Size.Y-1) * gfFemaleSize
 	zOfs := -0.5 * h
 	idx := 0
-	for i := 0; i < k.X; i++ {
-		for j := 0; j < k.Y; j++ {
+	for i := 0; i < k.Size.X; i++ {
+		for j := 0; j < k.Size.Y; j++ {
 			posn[idx] = v3.Vec{xOfs + float64(i)*gfFemaleSize, yOfs + float64(j)*gfFemaleSize, zOfs}
 			idx++
 		}
 	}
+
+	// base plugs
 	plugs := sdf.Multi3D(gfMale(), posn)
 
+	// through holes
+	var holes sdf.SDF3
+	if k.Hole {
+		holes = sdf.Multi3D(gfThruHoles(h+gfMaleHeight-gfHoleHeight), posn)
+	}
+
 	// stacking lip
-	lip := gfLip(size.X, size.Y)
+	empty := 0.0
+	if k.Empty {
+		empty = h - gfLipHeight - gfFloor
+	}
+	lip := gfLip(size.X, size.Y, empty)
 	lip = sdf.Transform3D(lip, sdf.Translate3d(v3.Vec{0, 0, 0.5 * h}))
 
-	return sdf.Difference3D(sdf.Union3D(body, plugs), lip)
+	return sdf.Difference3D(sdf.Union3D(body, plugs), sdf.Union3D(lip, holes))
 }
 
 //-----------------------------------------------------------------------------
