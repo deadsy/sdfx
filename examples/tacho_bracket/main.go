5a818a131dbd603e8a0e041a555f00570361fc0f
Jason Harris <sirmanlypowers@gmail.com>
2023-05-04 17:34:30 -0700

    Completed tachometer bracket design.

--- examples/tacho_bracket/main.go
+++ examples/tacho_bracket/main.go
@@ -14,37 +14,68 @@ import (
 	"github.com/deadsy/sdfx/render"
 	"github.com/deadsy/sdfx/sdf"
 	v2 "github.com/deadsy/sdfx/vec/v2"
+	v3 "github.com/deadsy/sdfx/vec/v3"
 )
 
 //-----------------------------------------------------------------------------
 
 const tachoRadius = 0.5 * 3.5 * sdf.MillimetresPerInch
-const bracketHeight = 10.0
+const bracketHeight = 15.0
 const bracketWidth = 10.0
-const tabWidth = 30.0
-const tabLength = 30.0
+const tabWidth = 20.0
+const tabLength = 20.0
+const slotWidth = 4.0
+const screwRadius = 1.1 * 0.5 * (5.0 / 32.0) * sdf.MillimetresPerInch
 
 //-----------------------------------------------------------------------------
 
 func tachoBracket() (sdf.SDF3, error) {
 
+	// outer bracket
 	const outerRadius = tachoRadius + bracketWidth
-
-	s0, err := sdf.Circle2D(outerRadius)
+	body, err := sdf.Circle2D(outerRadius)
 	if err != nil {
 		return nil, err
 	}
 
-	s1, err := sdf.Circle2D(tachoRadius)
+	// inner hole
+	hole, err := sdf.Circle2D(tachoRadius)
 	if err != nil {
 		return nil, err
 	}
 
-	s2 := sdf.Box2D(v2.Vec{2.0 * (outerRadius + tabLength), tabWidth}, 0.1*(tabWidth+tabLength))
+	// side tabs
+	tabs := sdf.Box2D(v2.Vec{2.0 * (outerRadius + tabLength), tabWidth}, 0.07*(tabWidth+tabLength))
 
-	s3 := sdf.Difference2D(sdf.Union2D(s0, s2), s1)
+	// slot
+	l := bracketWidth + tabLength
+	slot := sdf.Box2D(v2.Vec{l, slotWidth}, 0)
+	slot = sdf.Transform2D(slot, sdf.Translate2d(v2.Vec{0.5*l + tachoRadius, 0}))
 
-	return sdf.Extrude3D(s3, bracketHeight), nil
+	// panel hole
+	panelHole, err := sdf.Circle2D(screwRadius)
+	if err != nil {
+		return nil, err
+	}
+	const xOfs = tachoRadius + bracketWidth + 0.5*tabLength
+	panelHole = sdf.Transform2D(panelHole, sdf.Translate2d(v2.Vec{-xOfs, 0}))
+
+	// outer body
+	s3 := sdf.Union2D(body, tabs)
+	s3.(*sdf.UnionSDF2).SetMin(sdf.PolyMin(bracketWidth))
+
+	// remove the holes
+	s4 := sdf.Difference2D(s3, sdf.Union2D(hole, slot, panelHole))
+	bracket := sdf.Extrude3D(s4, bracketHeight)
+
+	// clamp hole
+	clampHole, err := sdf.Cylinder3D(1.1*tabWidth, screwRadius, 0)
+	clampHole = sdf.Transform3D(clampHole, sdf.RotateX(0.5*sdf.Pi))
+	clampHole = sdf.Transform3D(clampHole, sdf.Translate3d(v3.Vec{xOfs, 0, 0}))
+
+	bracket = sdf.Difference3D(bracket, clampHole)
+
+	return bracket, nil
 }
 
 //-----------------------------------------------------------------------------
