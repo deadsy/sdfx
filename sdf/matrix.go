b6bcf5ce6921dfa25d479a00eef7a4975ed31c98
Megidd Git <megiddgit@gmail.com>
2023-05-08 06:01:30 -0700

    Get & set methods for 4x4 matrix

--- sdf/matrix.go
+++ sdf/matrix.go
@@ -568,3 +568,108 @@ func (a M22) Inverse() M22 {
 }
 
 //-----------------------------------------------------------------------------
+
+// Get a component of the matrix.
+func (a M44) Get(i, j int) float64 {
+	switch i {
+	case 0:
+		switch j {
+		case 0:
+			return a.x00
+		case 1:
+			return a.x01
+		case 2:
+			return a.x02
+		case 3:
+			return a.x03
+		}
+	case 1:
+		switch j {
+		case 0:
+			return a.x10
+		case 1:
+			return a.x11
+		case 2:
+			return a.x12
+		case 3:
+			return a.x13
+		}
+	case 2:
+		switch j {
+		case 0:
+			return a.x20
+		case 1:
+			return a.x21
+		case 2:
+			return a.x22
+		case 3:
+			return a.x23
+		}
+	case 3:
+		switch j {
+		case 0:
+			return a.x30
+		case 1:
+			return a.x31
+		case 2:
+			return a.x32
+		case 3:
+			return a.x33
+		}
+	}
+	panic("bad component")
+}
+
+// Set a component of the matrix.
+func (a *M44) Set(i, j int, val float64) {
+	switch i {
+	case 0:
+		switch j {
+		case 0:
+			a.x00 = val
+		case 1:
+			a.x01 = val
+		case 2:
+			a.x02 = val
+		case 3:
+			a.x03 = val
+		}
+	case 1:
+		switch j {
+		case 0:
+			a.x10 = val
+		case 1:
+			a.x11 = val
+		case 2:
+			a.x12 = val
+		case 3:
+			a.x13 = val
+		}
+	case 2:
+		switch j {
+		case 0:
+			a.x20 = val
+		case 1:
+			a.x21 = val
+		case 2:
+			a.x22 = val
+		case 3:
+			a.x23 = val
+		}
+	case 3:
+		switch j {
+		case 0:
+			a.x30 = val
+		case 1:
+			a.x31 = val
+		case 2:
+			a.x32 = val
+		case 3:
+			a.x33 = val
+		}
+	default:
+		panic("bad component")
+	}
+}
+
+//-----------------------------------------------------------------------------
