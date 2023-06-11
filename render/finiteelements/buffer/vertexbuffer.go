be371d4b3c14cb05546ccabfaccd35b8fc194be5
Megidd Git <megiddgit@gmail.com>
2023-06-11 12:37:37 +0330

    Fix bug: repeated tetrahedron nodes

--- render/finiteelements/buffer/vertexbuffer.go
+++ render/finiteelements/buffer/vertexbuffer.go
@@ -11,14 +11,14 @@ import (
 // The same vertex buffer is used for 4-node tetrahedra, 8-node hexahedra, and others.
 type VB struct {
 	// To store index of vertices. Repeated vertices would have the same index.
-	hashTable map[[3]int32]uint32
+	hashTable map[[3]float64]uint32
 	// To store coordinates of vertices.
 	V []v3.Vec
 }
 
 func NewVB() *VB {
 	b := VB{
-		hashTable: map[[3]int32]uint32{},
+		hashTable: map[[3]float64]uint32{},
 		V:         []v3.Vec{},
 	}
 
@@ -29,9 +29,10 @@ func NewVB() *VB {
 // If vertex is already available on the buffer, its ID is just returned.
 // So, all vertices will be unique. Not repeated.
 func (b *VB) Id(v v3.Vec) uint32 {
-	// Deduplicate by removing small details and use of epsilon
-	epsilon := float64(0.0001)
-	key := [3]int32{int32((v.X + epsilon) * 1000), int32((v.Y + epsilon) * 1000), int32((v.Z + epsilon) * 1000)}
+	// Do NOT remove any small details. Small details matter.
+	// Removing small details would cause this bug:
+	// https://calculix.discourse.group/t/detect-bad-finite-elements-4-node-tetrahedral/1700/5?u=megidd
+	key := [3]float64{v.X, v.Y, v.Z}
 	if vID, ok := b.hashTable[key]; ok {
 		// Vertex already exists. It's repeated.
 		return vID
