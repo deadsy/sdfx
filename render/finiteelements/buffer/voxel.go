e61904fe8345cdd731f190709633cdce38d489f6
Megidd Git <megiddgit@gmail.com>
2023-06-13 09:22:18 +0330

    Store elements in voxel grid by their pointer

--- render/finiteelements/buffer/voxel.go
+++ render/finiteelements/buffer/voxel.go
@@ -15,13 +15,13 @@ func NewElement(nodes []uint32) *Element {
 // a one-dimensional slice under the hood.
 // To increase performance.
 type VoxelGrid struct {
-	data             [][]Element // Each voxel stores multiple elements.
-	lenX, lenY, lenZ int         // Voxels count in 3 directions.
+	data             [][]*Element // Each voxel stores multiple elements.
+	lenX, lenY, lenZ int          // Voxels count in 3 directions.
 }
 
 func NewVoxelGrid(x, y, z int) *VoxelGrid {
 	return &VoxelGrid{
-		data: make([][]Element, x*y*z),
+		data: make([][]*Element, x*y*z),
 		lenX: x,
 		lenY: y,
 		lenZ: z,
@@ -29,16 +29,16 @@ func NewVoxelGrid(x, y, z int) *VoxelGrid {
 }
 
 // To get all the elements inside a voxel.
-func (vg *VoxelGrid) Get(x, y, z int) []Element {
+func (vg *VoxelGrid) Get(x, y, z int) []*Element {
 	return vg.data[x*vg.lenY*vg.lenZ+y*vg.lenZ+z]
 }
 
 // To set all the elements inside a voxel at once.
-func (vg *VoxelGrid) Set(x, y, z int, value []Element) {
+func (vg *VoxelGrid) Set(x, y, z int, value []*Element) {
 	vg.data[x*vg.lenY*vg.lenZ+y*vg.lenZ+z] = value
 }
 
 // To append a single element to the elements inside a voxel.
-func (vg *VoxelGrid) Append(x, y, z int, value Element) {
+func (vg *VoxelGrid) Append(x, y, z int, value *Element) {
 	vg.data[x*vg.lenY*vg.lenZ+y*vg.lenZ+z] = append(vg.data[x*vg.lenY*vg.lenZ+y*vg.lenZ+z], value)
 }
