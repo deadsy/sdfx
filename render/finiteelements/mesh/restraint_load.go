1d1917324422bdd7ce33761b8b8f4699c9ed5b19
Megidd Git <megiddgit@gmail.com>
2023-07-16 09:55:19 +0330

    New file for restraint & load

---
+++ render/finiteelements/mesh/restraint_load.go
@@ -0,0 +1,44 @@
+package mesh
+
+import (
+	v3 "github.com/deadsy/sdfx/vec/v3"
+	"github.com/deadsy/sdfx/vec/v3i"
+)
+
+// Single point constraint: one or more degrees of freedom are fixed for a given node.
+type Restraint struct {
+	Location v3.Vec  // Exact coordinate.
+	IsFixedX bool    // Is X degree of freedom fixed?
+	IsFixedY bool    // Is Y degree of freedom fixed?
+	IsFixedZ bool    // Is Z degree of freedom fixed?
+	voxel    v3i.Vec // Containing voxel: to be computed by logic.
+	nodeID   int     // Eventual node to which the restraint is applied. To be computed.
+}
+
+// Point loads are applied to the nodes of the mesh.
+type Load struct {
+	Location  v3.Vec  // Exact coordinate.
+	Magnitude v3.Vec  // X, Y, Z magnitude.
+	voxel     v3i.Vec // Containing voxel: to be computed by logic.
+	nodeID    int     // Eventual node to which the load is applied. To be computed.
+}
+
+func NewRestraint(location v3.Vec, isFixedX, isFixedY, isFixedZ bool) *Restraint {
+	return &Restraint{
+		Location: location,
+		IsFixedX: isFixedX,
+		IsFixedY: isFixedY,
+		IsFixedZ: isFixedZ,
+		voxel:    v3i.Vec{},
+		nodeID:   0,
+	}
+}
+
+func NewLoad(location, magnitude v3.Vec) *Load {
+	return &Load{
+		Location:  location,
+		Magnitude: magnitude,
+		voxel:     v3i.Vec{},
+		nodeID:    0,
+	}
+}
