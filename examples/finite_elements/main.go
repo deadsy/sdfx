975c7e7914e5bce834947afcf39fca94cf90e840
Megidd Git <megiddgit@gmail.com>
2023-07-22 13:49:18 +0330

    Run example according to element config

--- examples/finite_elements/main.go
+++ examples/finite_elements/main.go
@@ -23,7 +23,6 @@ import (
 	v3 "github.com/deadsy/sdfx/vec/v3"
 )
 
-// Declare the enum using iota and const
 type Benchmark int
 
 const (
@@ -34,11 +33,23 @@ const (
 	Unknown
 )
 
+type ElementConfig int
+
+const (
+	Tet4       ElementConfig = iota + 1 // tet4 i.e. 4-node tetrahedron
+	Tet10                               // tet10 i.e. 10-node tetrahedron
+	Hex8                                // hex8 i.e. 8-node hexahedron
+	Hex20                               // hex20 i.e. 20-node hexahedron
+	Hex8Tet4                            // hex8 and tet4
+	Hex20Tet10                          // hex20 and tet10
+)
+
 // Render SDF3 to finite elements.
 // Write finite elements to an `inp` file.
 // Written file can be used by ABAQUS or CalculiX.
 func main() {
 	benchmark := Square
+	elementconfig := Hex20Tet10
 
 	// Optional argument from 1 to 4 to specify the benchmark to run.
 	if len(os.Args) > 1 {
@@ -47,6 +58,15 @@ func main() {
 			log.Fatalf("error: %s", err)
 		}
 		benchmark = Benchmark(bmint)
+	}
+
+	// Optional argument from 1 to 6 to specify the elements to generate.
+	if len(os.Args) > 2 {
+		ecint, err := strconv.Atoi(os.Args[2])
+		if err != nil {
+			log.Fatalf("error: %s", err)
+		}
+		elementconfig = ElementConfig(ecint)
 	}
 
 	// To be set for each benchmark.
@@ -61,14 +81,14 @@ func main() {
 	switch benchmark {
 	case Square:
 		restraints = benchmarkSquareRestraint()
-		benchmarkRun("../../files/benchmark-square.stl", 50, 0, 3, restraints, loads)
+		benchmarkRun("../../files/benchmark-square.stl", 50, 0, 3, restraints, loads, elementconfig)
 	case Circle:
 		restraints = benchmarkCircleRestraint()
-		benchmarkRun("../../files/benchmark-circle.stl", 50, 0, 3, restraints, loads)
+		benchmarkRun("../../files/benchmark-circle.stl", 50, 0, 3, restraints, loads, elementconfig)
 	case Pipe:
-		benchmarkRun("../../files/benchmark-pipe.stl", 50, 0, 3, restraints, loads)
+		benchmarkRun("../../files/benchmark-pipe.stl", 50, 0, 3, restraints, loads, elementconfig)
 	case I:
-		benchmarkRun("../../files/benchmark-I.stl", 50, 0, 3, restraints, loads)
+		benchmarkRun("../../files/benchmark-I.stl", 50, 0, 3, restraints, loads, elementconfig)
 	default:
 	}
 }
@@ -81,6 +101,7 @@ func benchmarkRun(
 	layerStart, layerEnd int,
 	restraints []*mesh.Restraint,
 	loads []*mesh.Load,
+	elementconfig ElementConfig,
 ) {
 	// read the stl file.
 	file, err := os.OpenFile(stl, os.O_RDONLY, 0400)
@@ -94,96 +115,100 @@ func benchmarkRun(
 		log.Fatalf("error: %s", err)
 	}
 
-	// tet4 i.e. 4-node tetrahedron
-	err = fe(inSdf, resolution, render.Linear, render.Tetrahedral, "tet4.inp", restraints, loads)
-	if err != nil {
-		log.Fatalf("error: %s", err)
-	}
+	switch elementconfig {
+	case Tet4:
+		{
 
-	// tet4 i.e. 4-node tetrahedron
-	err = feLayers(
-		inSdf, resolution, render.Linear, render.Tetrahedral,
-		fmt.Sprintf("tet4--layers-%v-to-%v.inp", layerStart, layerEnd),
-		restraints, loads, layerStart, layerEnd,
-	)
-	if err != nil {
-		log.Fatalf("error: %s", err)
-	}
+			err = fe(inSdf, resolution, render.Linear, render.Tetrahedral, "tet4.inp", restraints, loads)
+			if err != nil {
+				log.Fatalf("error: %s", err)
+			}
 
-	// tet10 i.e. 10-node tetrahedron
-	err = fe(inSdf, resolution, render.Quadratic, render.Tetrahedral, "tet10.inp", restraints, loads)
-	if err != nil {
-		log.Fatalf("error: %s", err)
-	}
+			err = feLayers(
+				inSdf, resolution, render.Linear, render.Tetrahedral,
+				fmt.Sprintf("tet4--layers-%v-to-%v.inp", layerStart, layerEnd),
+				restraints, loads, layerStart, layerEnd,
+			)
+			if err != nil {
+				log.Fatalf("error: %s", err)
+			}
+		}
+	case Tet10:
+		{
+			err = fe(inSdf, resolution, render.Quadratic, render.Tetrahedral, "tet10.inp", restraints, loads)
+			if err != nil {
+				log.Fatalf("error: %s", err)
+			}
 
-	// tet10 i.e. 10-node tetrahedron
-	err = feLayers(
-		inSdf, resolution, render.Quadratic, render.Tetrahedral,
-		fmt.Sprintf("tet10--layers-%v-to-%v.inp", layerStart, layerEnd),
-		restraints, loads, layerStart, layerEnd,
-	)
-	if err != nil {
-		log.Fatalf("error: %s", err)
-	}
+			err = feLayers(
+				inSdf, resolution, render.Quadratic, render.Tetrahedral,
+				fmt.Sprintf("tet10--layers-%v-to-%v.inp", layerStart, layerEnd),
+				restraints, loads, layerStart, layerEnd,
+			)
+			if err != nil {
+				log.Fatalf("error: %s", err)
+			}
+		}
+	case Hex8:
+		{
+			err = fe(inSdf, resolution, render.Linear, render.Hexahedral, "hex8.inp", restraints, loads)
+			if err != nil {
+				log.Fatalf("error: %s", err)
+			}
 
-	// hex8 i.e. 8-node hexahedron
-	err = fe(inSdf, resolution, render.Linear, render.Hexahedral, "hex8.inp", restraints, loads)
-	if err != nil {
-		log.Fatalf("error: %s", err)
-	}
+			err = feLayers(inSdf, resolution, render.Linear, render.Hexahedral,
+				fmt.Sprintf("hex8--layers-%v-to-%v.inp", layerStart, layerEnd),
+				restraints, loads, layerStart, layerEnd,
+			)
+			if err != nil {
+				log.Fatalf("error: %s", err)
+			}
+		}
+	case Hex20:
+		{
+			err = fe(inSdf, resolution, render.Quadratic, render.Hexahedral, "hex20.inp", restraints, loads)
+			if err != nil {
+				log.Fatalf("error: %s", err)
+			}
 
-	// hex8 i.e. 8-node hexahedron
-	err = feLayers(inSdf, resolution, render.Linear, render.Hexahedral,
-		fmt.Sprintf("hex8--layers-%v-to-%v.inp", layerStart, layerEnd),
-		restraints, loads, layerStart, layerEnd,
-	)
-	if err != nil {
-		log.Fatalf("error: %s", err)
-	}
+			err = feLayers(inSdf, resolution, render.Quadratic, render.Hexahedral,
+				fmt.Sprintf("hex20--layers-%v-to-%v.inp", layerStart, layerEnd),
+				restraints, loads, layerStart, layerEnd,
+			)
+			if err != nil {
+				log.Fatalf("error: %s", err)
+			}
+		}
+	case Hex8Tet4:
+		{
+			err = fe(inSdf, resolution, render.Linear, render.Both, "hex8tet4.inp", restraints, loads)
+			if err != nil {
+				log.Fatalf("error: %s", err)
+			}
 
-	// hex20 i.e. 20-node hexahedron
-	err = fe(inSdf, resolution, render.Quadratic, render.Hexahedral, "hex20.inp", restraints, loads)
-	if err != nil {
-		log.Fatalf("error: %s", err)
-	}
+			err = feLayers(inSdf, resolution, render.Linear, render.Both,
+				fmt.Sprintf("hex8tet4--layers-%v-to-%v.inp", layerStart, layerEnd),
+				restraints, loads, layerStart, layerEnd,
+			)
+			if err != nil {
+				log.Fatalf("error: %s", err)
+			}
+		}
+	case Hex20Tet10:
+		{
+			err = fe(inSdf, resolution, render.Quadratic, render.Both, "hex20tet10.inp", restraints, loads)
+			if err != nil {
+				log.Fatalf("error: %s", err)
+			}
 
-	// hex20 i.e. 20-node hexahedron
-	err = feLayers(inSdf, resolution, render.Quadratic, render.Hexahedral,
-		fmt.Sprintf("hex20--layers-%v-to-%v.inp", layerStart, layerEnd),
-		restraints, loads, layerStart, layerEnd,
-	)
-	if err != nil {
-		log.Fatalf("error: %s", err)
-	}
-
-	// hex8 and tet4
-	err = fe(inSdf, resolution, render.Linear, render.Both, "hex8tet4.inp", restraints, loads)
-	if err != nil {
-		log.Fatalf("error: %s", err)
-	}
-
-	// hex8 and tet4
-	err = feLayers(inSdf, resolution, render.Linear, render.Both,
-		fmt.Sprintf("hex8tet4--layers-%v-to-%v.inp", layerStart, layerEnd),
-		restraints, loads, layerStart, layerEnd,
-	)
-	if err != nil {
-		log.Fatalf("error: %s", err)
-	}
-
-	// hex20 and tet10
-	err = fe(inSdf, resolution, render.Quadratic, render.Both, "hex20tet10.inp", restraints, loads)
-	if err != nil {
-		log.Fatalf("error: %s", err)
-	}
-
-	// hex20 and tet10
-	err = feLayers(inSdf, resolution, render.Quadratic, render.Both,
-		fmt.Sprintf("hex20tet10--layers-%v-to-%v.inp", layerStart, layerEnd),
-		restraints, loads, layerStart, layerEnd,
-	)
-	if err != nil {
-		log.Fatalf("error: %s", err)
+			err = feLayers(inSdf, resolution, render.Quadratic, render.Both,
+				fmt.Sprintf("hex20tet10--layers-%v-to-%v.inp", layerStart, layerEnd),
+				restraints, loads, layerStart, layerEnd,
+			)
+			if err != nil {
+				log.Fatalf("error: %s", err)
+			}
+		}
 	}
 }
 
